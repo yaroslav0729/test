224fab4257f48da85be0c90d8114d16b642e050d
Kirill Vorotilov <kirill.vorotilov@greenice.net>
2020-10-16 15:52:28 +0300

    index page markup with npm modules

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 require('./bootstrap');
+
+require('popper.js');
+require('bootstrap');
+
+window.$ = require('jquery');
+window.jQuery = require('jquery');
+
+import Swiper from 'swiper';
+window.Swiper = Swiper
+
+require('bootstrap-input-spinner');
+
+require('./functions');
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,34 +5,35 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name') }}</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <script src="{{ asset('js/app.js') }}""></script>
 
-        <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+        {{-- <script src="https://code.jquery.com/jquery-3.3.1.js"></script> --}}
+        {{-- <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css"> --}}
 
-        <link rel="stylesheet" href="libs/bootstrap/css/bootstrap.css">
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-        <script src="/libs/bootstrap/js/bootstrap.js"></script>
+        {{-- <link rel="stylesheet" href="/libs/bootstrap/css/bootstrap.css"> --}}
+        {{-- <script src="/libs/bootstrap/js/bootstrap.js"></script> --}}
+        {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script> --}}
+        
+        {{-- <script src="/libs/bootstrap-input-spinner.js"></script> --}}
+        
+        {{-- <link href="/libs/fontawesome-pro-5.14.0/css/all.css" rel="stylesheet"> --}}
+        {{-- <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
+        <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script> --}}
 
-        <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 
+        {{-- <script src="/js/functions.js"></script> --}}
 
-        <link href="/libs/fontawesome-pro-5.14.0/css/all.css" rel="stylesheet">
-        <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
-        <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
-
-        <script src="/libs/bootstrap-input-spinner.js"></script>
-
-        <script src="/js/functions.js"></script>
         <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Serif:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap" rel="stylesheet">
 
-        <link type="text/css" href="css/styles.css?1" rel="stylesheet" media="all" />
+        {{-- <link type="text/css" href="{{ asset('css/styles.css') }}" rel="stylesheet" media="all" /> --}}
 
         @yield('head')
     </head>
