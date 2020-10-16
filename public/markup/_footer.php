d9db8b4bca8cc201added9d8b0944231075e2e59
Ivan Galushka <i.galushka@greenice.net>
2020-10-16 12:39:21 +0300

    markup payment details

--- public/markup/_footer.php
+++ public/markup/_footer.php
@@ -50,31 +50,33 @@
                     </div>
                 </div>
                 <div class="black-line"></div>
-                <div class="item">
-                    <div class="row">
-                        <div class="col-8">
-                            <p class="font-size-20 mb-0"><b>General Charity</b></p>
-                            <p class="font-size-20 mb-0">Single payment</p>
+                <div class="order-cart-list">
+                    <div class="item">
+                        <div class="row">
+                            <div class="col-8">
+                                <p class="font-size-20 mb-0"><b>General Charity</b></p>
+                                <p class="font-size-20 mb-0">Single payment</p>
+                            </div>
+                            <div class="col-4 text-right"><a href="#" class="btn-remove"> <i class="fal fa-times"></i> REMOVE</a></div>
                         </div>
-                        <div class="col-4 text-right"><a href="#" class="btn-remove"> <i class="fal fa-times"></i> REMOVE</a></div>
-                    </div>
-                    <div class="row align-items-center">
-                        <div class="col-8"><p class="font-size-20 mb-0"><b>£50.00</b></p></div>
-                        <div class="col-4 text-right"><input type="number" value="1" min="0" max="1000" step="1" class="color-danger"/></div>
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="row">
-                        <div class="col-8">
-                            <p class="font-size-20 mb-0"><b>General Charity</b></p>
-                            <p class="font-size-20 mb-0">Single payment</p>
-                            <span>+Sadiqah</span>
+                        <div class="row align-items-center">
+                            <div class="col-8"><p class="font-size-20 mb-0"><b>£50.00</b></p></div>
+                            <div class="col-4 text-right"><input type="number" value="1" min="0" max="1000" step="1" class="color-danger"/></div>
                         </div>
-                        <div class="col-4 text-right"><a href="#" class="btn-remove"> <i class="fal fa-times"></i> REMOVE</a></div>
                     </div>
-                    <div class="row align-items-center">
-                        <div class="col-8"><p class="font-size-20 mb-0"><b>£50.00</b></p></div>
-                        <div class="col-4 text-right"><input type="number" value="1" min="0" max="1000" step="1" class="color-danger"/></div>
+                    <div class="item">
+                        <div class="row">
+                            <div class="col-8">
+                                <p class="font-size-20 mb-0"><b>General Charity</b></p>
+                                <p class="font-size-20 mb-0">Single payment</p>
+                                <span>+Sadiqah</span>
+                            </div>
+                            <div class="col-4 text-right"><a href="#" class="btn-remove"> <i class="fal fa-times"></i> REMOVE</a></div>
+                        </div>
+                        <div class="row align-items-center">
+                            <div class="col-8"><p class="font-size-20 mb-0"><b>£50.00</b></p></div>
+                            <div class="col-4 text-right"><input type="number" value="1" min="0" max="1000" step="1" class="color-danger"/></div>
+                        </div>
                     </div>
                 </div>
 
