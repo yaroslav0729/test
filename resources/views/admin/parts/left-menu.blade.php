2a23403f14764e82b03e4f4a2d904e072a207391
Kirill Vorotilov <kirill.vorotilov@greenice.net>
2020-10-19 10:15:49 +0300

    admin styles

--- resources/views/admin/parts/left-menu.blade.php
+++ resources/views/admin/parts/left-menu.blade.php
@@ -1,7 +1,9 @@
-<div id="left_menu" class="text-gray-700 bg-gray-400 px-4 py-2 m-2">
+<div id="left_menu" class="text-gray-700 bg-gray-500 px-4 py-2 m-2">
     <ul>
         <li><a href="{{ route('admin.index') }}"><i class="fas fa-user-friends"></i>Users</a></li>
         <li><a href="{{ route('admin.post.index') }}"><i class="fas fa-file-alt"></i>Posts</a></li>
+        <li><a href="{{ route('admin.index') }}"><i class="fas fa-book"></i>Categories</a></li>
+        <li><a href="{{ route('admin.index') }}"><i class="fas fa-photo-video"></i>Media</a></li>
         <li><a href="{{ route('admin.index') }}"><i class="fas fa-sliders-h"></i>Settings</a></li>
     </ul>
     <br><br>
@@ -9,7 +11,7 @@
     <form method="POST" action="{{ route('logout') }}">
         @csrf
 
-        <button class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+        <button class="bg-gray-600 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
             Logout
         </button>
     </form>
