c9596811f145d5d962de283c22bf6c0d65464757
Kirill Vorotilov <kirill.vorotilov@greenice.net>
2020-10-21 09:47:54 +0300

    save widgets on post created, delete post-widgets, post-groups if post deleted

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -28,6 +28,16 @@ class Post extends Model
         return $this->hasMany('App\Models\PostItem')->orderBy('ordering');
     }
 
+    public static function boot()
+    {
+        parent::boot();
+
+        self::deleting(function($model){
+            $model->groups()->detach();
+            $model->widgets()->delete();
+        });
+    }
+
     public function getGroupIdsAttribute()
     {
         return $this->groups->pluck('id')->toArray();
