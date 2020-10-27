736803c0b65a9384f5536973744e243a152e0354
Ivan Galushka <i.galushka@greenice.net>
2020-10-27 14:26:02 +0300

    markup

---
+++ public/markup/mobile/project.php
@@ -0,0 +1,310 @@
+<?php include "_header.php"; ?>
+
+<div class="pt-4"></div>
+<div class="pt-5"></div>
+
+<section class="donate-today blue-gradient">
+    <div class="wrap">
+        <div class="title mb-5">
+            <p class="font-size-40"><b>Snappy Project Title/CTA 30ch</b></p>
+            <i class="far fa-arrow-down"></i>
+        </div>
+        <div class="pt-5"></div>
+
+        <div class="body">
+            <div class="row gutter-0">
+                <div class="col-6">
+                    <div class="media">
+                        <img src="img/content/project-1.jpg" alt="" class="w-100">
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="donate-today-sheet">
+
+                        <nav>
+                            <div class="nav nav-tabs nav-fill" id="nav-tab" role="tablist">
+                                <a class="nav-link active color-info"  data-toggle="tab" href="#nav-1" role="tab" aria-selected="true">Single Donation</a>
+                                <a class="nav-link color-info"  data-toggle="tab" href="#nav-2" role="tab"  aria-selected="false">Monthly Donation</a>
+                            </div>
+                        </nav>
+                        <div class="tab-content" id="nav-tabContent">
+                            <div class="tab-pane fade show active" id="nav-1" role="tabpanel" >
+                                <form action="/">
+                                    <label class="item active-color-info">
+                                        <input type="radio" name="r1">
+                                        <span class="d-flex align-items-center">
+                                            <span><span>£<b>30</b></span><span>JUST ONCE</span></span>
+                                            <span>This donation could give a child a food and a blanket, to keep them warm this winter 90ch.</span>
+                                        </span>
+                                    </label>
+                                    <label class="item active-color-info">
+                                        <input type="radio" name="r1">
+                                        <span class="d-flex align-items-center">
+                                            <span><span>£<b>50</b></span><span>JUST ONCE</span></span>
+                                            <span>This donation could give a child a food and a blanket, to keep them warm this winter 90ch.</span>
+                                        </span>
+                                    </label>
+                                    <label class="item active-color-info">
+                                        <input type="radio" name="r1">
+                                        <span class="d-flex align-items-center">
+                                            <span><span>£<b>100</b></span><span>JUST ONCE</span></span>
+                                            <span>This donation could give a child a food and a blanket, to keep them warm this winter 90ch.</span>
+                                        </span>
+                                    </label>
+                                    <div class="pt-3"></div>
+                                    <div class="row gutter-5">
+                                        <div class="col-6">
+                                            <div class="form-group">
+                                                <select class="form-control">
+                                                    <option value="1">General Charity</option>
+                                                    <option value="1">General Charity 2</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="col-4">
+                                            <div class="form-group">
+                                                <input type="text" class="form-control" placeholder="£  Enter amount">
+                                            </div>
+                                        </div>
+                                        <div class="col-2">
+                                            <div class="form-group">
+                                                <select class="form-control">
+                                                    <option value="1">GPB</option>
+                                                    <option value="2">USD</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="pt-3"></div>
+                                    <div class="text-center">
+                                        <button class="btn btn-info border-white btn-submit">Donate</button>
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="tab-pane fade" id="nav-2" role="tabpanel" >
+                                <form action="/">
+                                    <label class="item active-color-info">
+                                        <input type="radio" name="r1">
+                                        <span class="d-flex align-items-center">
+                                            <span><span>£<b>30</b></span><span>JUST ONCE</span></span>
+                                            <span>This donation could give a child a food and a blanket, to keep them warm this winter 90ch.</span>
+                                        </span>
+                                    </label>
+                                    <label class="item active-color-info">
+                                        <input type="radio" name="r1">
+                                        <span class="d-flex align-items-center">
+                                            <span><span>£<b>50</b></span><span>JUST ONCE</span></span>
+                                            <span>This donation could give a child a food and a blanket, to keep them warm this winter 90ch.</span>
+                                        </span>
+                                    </label>
+                                    <label class="item active-color-info">
+                                        <input type="radio" name="r1">
+                                        <span class="d-flex align-items-center">
+                                            <span><span>£<b>100</b></span><span>JUST ONCE</span></span>
+                                            <span>This donation could give a child a food and a blanket, to keep them warm this winter 90ch.</span>
+                                        </span>
+                                    </label>
+                                    <div class="pt-3"></div>
+                                    <div class="row gutter-5">
+                                        <div class="col-6">
+                                            <div class="form-group">
+                                                <select class="form-control">
+                                                    <option value="1">General Charity</option>
+                                                    <option value="1">General Charity 2</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="col-4">
+                                            <div class="form-group">
+                                                <input type="text" class="form-control" placeholder="£  Enter amount">
+                                            </div>
+                                        </div>
+                                        <div class="col-2">
+                                            <div class="form-group">
+                                                <select class="form-control">
+                                                    <option value="1">GPB</option>
+                                                    <option value="2">USD</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="pt-3"></div>
+                                    <div class="text-center">
+                                        <button class="btn btn-info border-white btn-submit">Donate</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="descr"><div>Your subtitle/copy can go here, max of 100ch ut perspi unde omnis iste natus demiour sit voluptatem, abilloum inventore.</div></div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<div class="pt-5"></div>
+<div class="pt-5"></div>
+
+<section class="blog-article-body">
+    <div class="wrap">
+        <div class="body">
+            <h2>Why IH needs support CTA Title, to secure donation</h2>
+            <p>460 Characters perspiciais und omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicab. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed consequuntur magni dolores eos qui rati voluptate sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.</p>
+            <div class="pt-4"></div>
+            <h2>Inspiring CTA Title, of how your donation could help/solution</h2>
+            <p>460 Characters perspiciais und omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicab. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed consequuntur magni dolores eos qui rati voluptate sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.</p>
+
+            <div class="blog-video">
+                <div class="img-video play-tr" style="background-image: url(img/content/Video-placement-2.jpg)"><i class="fas fa-play-circle"></i></div>
+                <a href="#" class="view-more"><i class="far fa-arrow-right"></i></a>
+            </div>
+
+            <h2>What IH does with your donation for this project CTA</h2>
+            <p>460 Characters perspiciais und omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicab. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed consequuntur magni dolores eos qui rati voluptate sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.</p>
+
+
+        </div>
+    </div>
+</section>
+
+<div class="pt-5"></div>
+
+<div class="share-box">
+    <span>SHARE THIS</span>
+    <a href="#"><i class="fab fa-twitter"></i></a>
+    <a href="#"><i class="fab fa-facebook-f"></i></a>
+    <div class="stat"><span>1.2k</span></div>
+</div>
+
+
+<section class="whats-happened-far">
+    <p style="padding-left: 90px;" class="font-size-12 mb-4"><b>ISLAMIC HELP'S RESULTS</b></p>
+    <div class="black-line"></div>
+    <div class="body">
+        <div class="row gutter-0">
+            <div class="col-7" style="z-index: 2">
+                <div class="tl">What's happened so far.</div>
+                <div class="text bg-info">
+                    <p>180 Characters perspiciais und omnis iste natus error sit volup tatem accusantium dis doloremque laudantium, totam annum rem aperiam, eaque ipsa quae ab illomsi inventore veritatis.</p>
+                    <div class="info">
+                        <div class="bg bg-danger-light"></div>
+                        <div class="rectangle bg-danger"></div>
+                        <div class="help-info">
+                            <div>
+                                <span>8k</span>
+                                <span>People helped</span>
+                            </div>
+                            <div>
+                                <span>36</span>
+                                <span>Countries</span>
+                            </div>
+                            <div>
+                                <span>1'407</span>
+                                <span>Volunteers this year</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-5">
+                <img src="img/content/project-2.jpg" alt="">
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="donate-today-card">
+    <div class="wrap">
+        <div class="title">
+            <span>We still need your support.</span>
+        </div>
+        <div class="list d-flex justify-content-center">
+            <div class="item">
+                <div>£<b>30</b></div>
+                This donation<br>
+                could lorem analai<br>
+                dolor annum 50ch.
+            </div>
+            <div class="item active-color-info">
+                <div>£<b>60</b></div>
+                This donation<br>
+                could lorem analai<br>
+                dolor annum 50ch.
+            </div>
+            <div class="item active-color-danger">
+                <div>£<b>100</b></div>
+                This donation<br>
+                could lorem analai<br>
+                dolor annum 50ch.
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="join-cause">
+    <div class="wrap">
+        <div class="title mb-5">
+            <p class="font-size-30"><b>Important information / please note CTA</b></p>
+        </div>
+        <p class="font-size-20">
+            Perspiciais und omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicab. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed consequuntur magni dolores eos qui rati voluptate sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.
+        </p>
+    </div>
+</section>
+
+<section class="discover-more">
+    <div class="wrap">
+        <div class="title">
+            <div class="row">
+                <div class="col-7">
+                    <b class="font-size-30 mr-4 text-uppercase">Related topics</b>
+                </div>
+                <div class="col-5 text-right">
+                    <a href="#" class="text-uppercase text-underline"><b>visit newsroom</b> <i class="far fa-arrow-right"></i></a>
+                </div>
+            </div>
+        </div>
+
+        <div class="current-projects-list">
+            <div class="row">
+                <div class="col-4">
+                    <a href="#" class="item">
+                        <span class="img" style="background-image: url(img/content/discover-more-1.jpg)"></span>
+                        <span class="descr">
+                    <span class="name font-size-16">EVENT</span>
+                    <span class="text font-size-16"><b>Critical campaign title, 60 char lorem sit amet, demis.</b></span>
+                </span>
+                    </a>
+                </div>
+                <div class="col-4">
+                    <a href="#" class="item">
+                        <span class="img" style="background-image: url(img/content/discover-more-2.jpg)"></span>
+                        <span class="descr">
+                    <span class="name font-size-16">PROJECT</span>
+                    <span class="text font-size-16"><b>Critical campaign title, 60 char lorem sit amet, demis.</b></span>
+                </span>
+                    </a>
+                </div>
+                <div class="col-4">
+                    <a href="#" class="item">
+                        <span class="img" style="background-image: url(img/content/discover-more-3.jpg)"></span>
+                        <span class="descr">
+                    <span class="name font-size-16">ARTICLE</span>
+                    <span class="text font-size-16"><b>Critical campaign title, 60 char lorem sit amet, demis.</b></span>
+                </span>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
+
+
+<?php include "_footer.php";?>
+
+</body>
+</html>
