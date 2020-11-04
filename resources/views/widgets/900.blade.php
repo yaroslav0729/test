7883ce3dfc6d931a7d10e74e7d17948269680387
Kirill Vorotilov <kirill.vorotilov@greenice.net>
2020-11-04 10:51:44 +0300

    video widget - iframe container

--- resources/views/widgets/900.blade.php
+++ resources/views/widgets/900.blade.php
@@ -4,7 +4,9 @@
             <div class="pt-4"></div>
 
             <div class="blog-video">
-                <div class="img-video play-tr" style="background-image: url(img/content/Video-placement-2.jpg)"><i class="fas fa-play-circle"></i></div>
+                <div class="img-video play-tr videoWrapper">
+                    <iframe width="500" height="350"  src="https://www.youtube.com/embed/XxkTM5fyabo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
                 <a href="#" class="view-more"><i class="far fa-arrow-right"></i></a>
             </div>
         </div>
