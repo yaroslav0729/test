1054c99ee58c45afa04ddd88ea9121f5dfbb283a
Kirill Vorotilov <kirill.vorotilov@greenice.net>
2020-11-04 11:08:28 +0300

    User roles seeder to database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use Database\Seeders\UserRolesSeeder;
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -9,10 +11,16 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      *
+     * php artisan db:seed
+     * 
      * @return void
      */
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        $this->call([
+            UserRolesSeeder::class,
+        ]);
     }
 }
