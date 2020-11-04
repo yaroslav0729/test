6ae6c5c2531d4034453907ffb5885ef6e90874b8
Kirill Vorotilov <kirill.vorotilov@greenice.net>
2020-11-04 11:00:19 +0300

    fix

--- resources/views/widgets/900.blade.php
+++ resources/views/widgets/900.blade.php
@@ -4,10 +4,13 @@
             <div class="pt-4"></div>
 
             <div class="blog-video">
-                <div class="img-video play-tr videoWrapper">
+                {{-- <div class="img-video play-tr videoWrapper">
                     <iframe width="500" height="350"  src="https://www.youtube.com/embed/XxkTM5fyabo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                </div>
+                </div> --}}
+
+                <div class="img-video play-tr" style="background-image: url(img/content/Video-placement-2.jpg)"><i class="fas fa-play-circle"></i></div>
                 <a href="#" class="view-more"><i class="far fa-arrow-right"></i></a>
+
             </div>
         </div>
     </div>
