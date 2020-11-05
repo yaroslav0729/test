985110e5c2171fad8b494389ed287844bc6f0f05
Kirill Vorotilov <kirill.vorotilov@greenice.net>
2020-11-05 19:46:06 +0300

    parameters form render - fix wysiwyg

--- resources/js/app.js
+++ resources/js/app.js
@@ -39,6 +39,8 @@ $(function () {
 
     function templateResponse(response) {
         $('#page_parameters').html(response.html);
+
+        initWysiwyg()
     }
 
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
