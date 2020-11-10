e78a69adb89739e62684bb6410e7b97a88372e2d
Kirill Vorotilov <kirill.vorotilov@greenice.net>
2020-11-10 17:39:47 +0300

    Save header parameters for index template

--- resources/views/templates/form/3.blade.php
+++ resources/views/templates/form/3.blade.php
@@ -1,32 +1,18 @@
 @php
 
-    $value1 = "";
-    $value2 = "";
-    $articleHtml = "";
+    $hdrType = "";
 
-    if (isset($parameters['param3'])) {
-        $value1 = $parameters['param3'];    
-    }
-
-    if (isset($parameters['param4'])) {
-        $value2 = $parameters['param4'];    
-    }
-
-    if (isset($parameters['article_html'])) {
-        $articleHtml = $parameters['article_html'];    
+    if (isset($parameters['hdr_type'])) {
+        $hdrType = $parameters['hdr_type'];    
     }
 
 @endphp
 
 <div class="form-group">
-    <label>Parameter 3</label>
-    <input class="form-control" placeholder="Test parameter 3" name="parameters[param3]" value="{{ $value1 }}" />
-</div>
-<div class="form-group">
-    <label>Parameter 4</label>
-    <input class="form-control" placeholder="Test parameter 4" name="parameters[param4]" value="{{ $value2 }}" />
-</div>
-<div class="form-group">
-    <label>Article html</label>
-    <textarea wysiwyg-editor class="form-control" id="article_html" name="parameters[article_html]">{{ $articleHtml }}</textarea>
+    <label>Header type</label>
+    <select name="parameters[hdr_type]" class="form-control">
+        @for ($i = 1; $i <= 5; $i++)
+            <option value="{{ $i }}" @if((int)($hdrType) === $i) selected @endif>Type {{ $i }}</option>
+        @endfor
+    </select>
 </div>
