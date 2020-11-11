262e3305a620a45156a6d9af17663916f2a53827
Kirill Vorotilov <kirill.vorotilov@greenice.net>
2020-11-11 10:45:42 +0300

    Index page - mobile version template

--- resources/views/templates/form/3.blade.php
+++ resources/views/templates/form/3.blade.php
@@ -5,6 +5,11 @@ $whoVideo = "";
 $whoLink = "";
 $whoTitle = "";
 $whoText = "";
+$longtermLink = "";
+$emergencyLink = "";
+$volunteeringLink = "";
+$sadiqahLink = "";
+
 
 if (isset($parameters['hdr_type'])) {
     $hdrType = $parameters['hdr_type'];    
@@ -24,6 +29,22 @@ if (isset($parameters['who_we_are_title'])) {
 
 if (isset($parameters['who_we_are_text'])) {
     $whoText = $parameters['who_we_are_text'];    
+}
+
+if (isset($parameters['our_work_longterm_link'])) {
+    $longtermLink = $parameters['our_work_longterm_link'];    
+}
+
+if (isset($parameters['our_work_emergency_link'])) {
+    $emergencyLink = $parameters['our_work_emergency_link'];    
+}
+
+if (isset($parameters['our_work_volunteering_link'])) {
+    $volunteeringLink = $parameters['our_work_volunteering_link'];    
+}
+
+if (isset($parameters['our_work_sadiqah_link'])) {
+    $sadiqahLink = $parameters['our_work_sadiqah_link'];    
 }
 
 @endphp
@@ -55,4 +76,24 @@ if (isset($parameters['who_we_are_text'])) {
 <div class="form-group">
     <label>Who we are text:</label>
     <textarea class="form-control" required name="parameters[who_we_are_text]" placeholder="Insert Who we are text">{{ $whoText }}</textarea>
+</div>
+
+<div class="form-group">
+    <label>Our work Longterm projects link:</label>
+    <input class="form-control" required name="parameters[our_work_longterm_link]" placeholder="Longterm projects" value="{{ $longtermLink }}" />
+</div>
+
+<div class="form-group">
+    <label>Our work Emergency relief link:</label>
+    <input class="form-control" required name="parameters[our_work_emergency_link]" placeholder="Emergency relief" value="{{ $emergencyLink }}" />
+</div>
+
+<div class="form-group">
+    <label>Our work Volunteering link:</label>
+    <input class="form-control" required name="parameters[our_work_volunteering_link]" placeholder="Who we are title" value="{{ $volunteeringLink }}" />
+</div>
+
+<div class="form-group">
+    <label>Our work Sadiqah link:</label>
+    <input class="form-control" required name="parameters[our_work_sadiqah_link]" placeholder="Who we are title" value="{{ $sadiqahLink }}" />
 </div>
