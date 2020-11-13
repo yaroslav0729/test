19b7de92633b90caeab677256c856bf7f5842fa8
Kirill Vorotilov <kirill.vorotilov@greenice.net>
2020-11-13 08:45:50 +0300

    Parser - replace images src attr to local storage path

--- app/Console/Commands/ConvertPages.php
+++ app/Console/Commands/ConvertPages.php
@@ -2,28 +2,26 @@
 
 namespace App\Console\Commands;
 
-use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Http;
-
-use voku\helper\HtmlDomParser;
 use App\Models\Category;
 use App\Models\Page;
-use App\Models\PageInstance;
+use App\Models\PageInstance; // https://github.com/voku/simple_html_dom
 use App\Models\Template;
-
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Storage;
+use voku\helper\HtmlDomParser;
 
 class ConvertPages extends Command
 {
     const PARSING_LINK = 'https://www.islamichelp.org.uk/media-centre-sitemap.xml';
-    const MEDIA_CENTER_IMAGE_STORAGE_PATH = '/media-center';
+    const MEDIA_CENTER_IMAGE_STORAGE_PATH = 'media-center';
 
     /**
      * The name and signature of the console command.
      *
-     * 
+     *
      * php artisan convert:media-center
-     * 
+     *
      * @var string
      */
     protected $signature = 'convert:media-center';
@@ -59,58 +57,46 @@ class ConvertPages extends Command
 
         foreach ($links as $key => $link) {
 
-            if ($key === 0) continue;
+            if ($key === 0) {
+                continue;
+            }
 
-            $arr = explode('/', $link);
-            $category = $arr[count($arr) - 3];
-            $slug = $arr[count($arr) - 2];
             $html = Http::get($link)->body();
-
             $document = new HtmlDomParser($html);
 
+            $category = $this->getCategoryFromLink($link);
+            $slug = $this->getSlugFromLink($link);
             $title = $this->searchTitle($html);
             $description = $this->searchDescription($html);
             $keywords = $this->searchKeywords($html);
-
             $h1 = $document->findOneOrFalse('h1')->text();
             $h2 = $document->findOneOrFalse('h2.title_post')->text();
             $date = $document->findOneOrFalse('div.date')->text();
-            
-            foreach ($document->find('h2.title_post') as $tag) {
-                $tag->outertext = '';
-            }
 
-            foreach ($document->find('div.date') as $tag) {
-                $tag->outertext = '';
-            }
-
-            foreach ($document->find('div.archive-news-sidebar') as $tag) {
-                $tag->outertext = '';
-            }
+            $this->removeElementFromDoc('h2.title_post', $document);
+            $this->removeElementFromDoc('div.date', $document);
+            $this->removeElementFromDoc('div.archive-news-sidebar', $document);
 
             $newsBlock = $document->findOneOrFalse('div#tpl-single-news');
             $newsHtml = $newsBlock->html();
 
             $images = $this->findAllImages($newsBlock);
             $images = $this->uploadAll($images);
-
-            if ($images !== []) {
-                dd($images);
-            }
+            $newsHtml = $this->replaceImagesLinks($newsHtml, $images);
 
             $category = Category::firstOrCreate([
                 'slug' => $category,
-                'name' => $category
+                'name' => $category,
             ]);
 
             $parameters = [];
             $parameters['main_html'] = $newsHtml;
 
             $pageInstance = PageInstance::where('slug', $slug)->where('actual', true)->first();
-            
+
             if (!isset($pageInstance)) {
                 $page = Page::create([
-                    'status' => Page::PAGE_STATUS_PUBLICHED
+                    'status' => Page::PAGE_STATUS_PUBLICHED,
                 ]);
                 $pageInstance = PageInstance::create([
                     'page_id' => $page->id,
@@ -143,27 +129,36 @@ class ConvertPages extends Command
                 $this->info('Page updated: slug ' . $pageInstance->slug);
             }
 
-            if ($key === 3) break;
+            if ($key === 3) {
+                break;
+            }
+
         }
 
         $this->info('Parsing process complete!!!');
     }
 
-    protected function uploadAll($images)
+    protected function removeElementFromDoc($element, $document)
     {
-        if ($images !== []) {
-            foreach ($images as $imageUrl) {
-                $this->info('image: ' . $imageUrl);
-                
-                $contents = file_get_contents($imageUrl);
-                $name = substr($imageUrl, strrpos($imageUrl, '/') + 1);
-                Storage::disk('public')->put(self::MEDIA_CENTER_IMAGE_STORAGE_PATH . '/' . $name, $contents);
-
-                $this->info('local image name: ' . $name);
-            }
+        foreach ($document->find($element) as $tag) {
+            $tag->outertext = '';
         }
+    }
 
-        return [];
+    protected function getCategoryFromLink($link)
+    {
+        $arr = explode('/', $link);
+        $category = $arr[count($arr) - 3];
+
+        return $category;
+    }
+
+    protected function getSlugFromLink($link)
+    {
+        $arr = explode('/', $link);
+        $slug = $arr[count($arr) - 2];
+
+        return $slug;
     }
 
     protected function getLinksFromXml($html)
@@ -179,24 +174,36 @@ class ConvertPages extends Command
     {
         preg_match_all('#<title>(.+?)</title>#su', $html, $res);
 
-        if (isset($res[1][0])) return $res[1][0];
-        else return "";
+        if (isset($res[1][0])) {
+            return $res[1][0];
+        } else {
+            return "";
+        }
+
     }
 
     protected function searchDescription($html)
     {
         preg_match_all('#<meta property="og:description" content="(.+?)/>#su', $html, $res);
 
-        if (isset($res[1][0])) return $res[1][0];
-        else return "";    
+        if (isset($res[1][0])) {
+            return $res[1][0];
+        } else {
+            return "";
+        }
+
     }
 
     protected function searchKeywords($html)
     {
         preg_match_all('#<meta name="keywords" content="(.+?)/>#su', $html, $res);
 
-        if (isset($res[1][0])) return $res[1][0];
-        else return "";  
+        if (isset($res[1][0])) {
+            return $res[1][0];
+        } else {
+            return "";
+        }
+
     }
 
     protected function findAllImages($el)
@@ -211,5 +218,39 @@ class ConvertPages extends Command
         }
 
         return $images;
+    }
+
+    protected function uploadAll($images)
+    {
+        $imagesLinks = [];
+
+        if ($images !== []) {
+            foreach ($images as $imageUrl) {
+                $this->info('image: ' . $imageUrl);
+
+                $contents = file_get_contents($imageUrl);
+                $name = substr($imageUrl, strrpos($imageUrl, '/') + 1);
+                $path = self::MEDIA_CENTER_IMAGE_STORAGE_PATH . '/' . $name;
+                Storage::disk('public')->put($path, $contents);
+
+                $this->info('local image name: ' . $name);
+
+                $imagesLinks[] = [
+                    'remote_image' => $imageUrl,
+                    'local_image' => Storage::url($path),
+                ];
+            }
+        }
+
+        return $imagesLinks;
+    }
+
+    protected function replaceImagesLinks($html, $images)
+    {
+        foreach ($images as $image) {
+            $html = str_replace($image['remote_image'], $image['local_image'], $html);
+        }
+
+        return $html;
     }
 }
