41659ac8961d6fec8a627cc1d72bbe5f9802e551
Kirill Vorotilov <kirill.vorotilov@greenice.net>
2020-11-13 10:29:43 +0300

    fix media-center pages markup

--- resources/views/templates/presentation/4.blade.php
+++ resources/views/templates/presentation/4.blade.php
@@ -13,13 +13,19 @@
 @endif
 
 <div class="container">
-    <h1 class="text-center">{{ $pageInstance->name }}</h1>
-    <h2>{{ $pageInstance->preview_text }}</h2>
-    <div class="date">
-        <i class="fas fa-calendar-alt"></i>
-        <span>{{ date('d F Y', strtotime($pageInstance->published_at)) }}</span>
+    <div class="row padding">
+        <div class="col-lg-12">
+            <h1 class="text-center">{{ $pageInstance->name }}</h1>
+            <h2>{{ $pageInstance->preview_text }}</h2>
+            <div class="date pt-4 pb-4">
+                <i class="fas fa-calendar-alt"></i>
+                <span>{{ date('d F Y', strtotime($pageInstance->published_at)) }}</span>
+            </div>
+        </div>
     </div>
+
     {!! $mainHtml !!}
+    
 </div>
 
 @include('modules.presentation.join_the_cause_subscribe')
