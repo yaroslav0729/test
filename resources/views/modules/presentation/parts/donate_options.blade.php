67d1048d32db1680356f257975f8ac4958803d19
Kirill Vorotilov <kirill.vorotilov@greenice.net>
2020-11-23 15:31:51 +0300

    donate module - campaigns price check

--- resources/views/modules/presentation/parts/donate_options.blade.php
+++ resources/views/modules/presentation/parts/donate_options.blade.php
@@ -1,14 +1,18 @@
 @foreach ($amount as $amountKey => $item)
     @if(isset($item['type']) && ((int)$item['type'] === $donateOptionsType))
-        <label class="item" select-amount data-amount_id={{ $amountKey }}>
-            <input type="radio" name="r1">
-            <span class="d-flex align-items-center">
-                <span><span>£<b>@isset($item['value']) {{ $item['value'] }} @endisset</b></span><span>JUST ONCE</span></span>
-                <span>@isset($item['text']) {{ $item['text'] }} @endisset</span>
-            </span>
-        </label>
         
         @isset($campaignsCountries[$amountKey])
+
+            @if(count($campaignsCountries[$amountKey])>0)
+            <label class="item" select-amount data-amount_id={{ $amountKey }}>
+                <input type="radio" name="r1">
+                <span class="d-flex align-items-center">
+                    <span><span>£<b>@isset($item['value']) {{ $item['value'] }} @endisset</b></span><span>JUST ONCE</span></span>
+                    <span>@isset($item['text']) {{ $item['text'] }} @endisset</span>
+                </span>
+            </label>
+            @endif
+
             <div class="form-group d-none" amount-countries data-countries_amount_id={{ $amountKey }}>
                 <select class="form-control">
                     <option>Country</option>
