7e58bd6e922292516ef945daec75530af6864ce0
Kirill Vorotilov <kirill.vorotilov@greenice.net>
2020-11-23 09:04:12 +0300

    Settings page style

--- resources/views/admin/settings/index.blade.php
+++ resources/views/admin/settings/index.blade.php
@@ -4,9 +4,35 @@
 
 <h2>Settings:</h2>
 
-<ul>
-    <li><a href="{{ route('admin.pages.edit', ['page' => $indexPage->page->id]) }}">Index page (Home)</a></li>
-    <li><a href="{{ route('admin.pages.edit', ['page' => $projectsPage->page->id]) }}">Projects page</a></li>
-</ul>
+<table class="table-auto mb-3">
+    <thead>
+      <tr>
+        <th class="px-2 w-100 py-2">Page name</th>
+        <th class="px-2 py-2">Action</th>
+      </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td class="border px-2 py-2">Index page</td>
+            <td class="border px-2 py-2">
+                <a href="{{ route('admin.pages.edit', ['page' => $indexPage->page->id]) }}">
+                    <button class="btn btn-info action-btn" type="submit" title="Edit page">
+                        <i class="fas fa-edit"></i>
+                    </button>
+                </a>
+            </td>
+        </tr>
+        <tr>
+            <td class="border px-2 py-2">Projects page</td>
+            <td class="border px-2 py-2">
+                <a href="{{ route('admin.pages.edit', ['page' => $indexPage->page->id]) }}">
+                    <button class="btn btn-info action-btn" type="submit" title="Edit page">
+                        <i class="fas fa-edit"></i>
+                    </button>
+                </a>
+            </td>
+        </tr>
+    </tbody>
+</table>
 
 @endsection
