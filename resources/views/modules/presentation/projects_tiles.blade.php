f2b3107dc2c5dffe14640b604dfb6570303f0f28
Kirill Vorotilov <kirill.vorotilov@greenice.net>
2020-11-23 21:18:20 +0300

    projects swiper

--- resources/views/modules/presentation/projects_tiles.blade.php
+++ resources/views/modules/presentation/projects_tiles.blade.php
@@ -1,29 +1,66 @@
-<div class="wrap">
-    <div class="row">
+<div class="current-projects-list w-100" swiper-wrapper="project-tiles">
+    <div class="swiper-container">
+        <div class="swiper-wrapper">
 
-        @foreach ($projects as $projKey => $project)
+                    @php
+                        $tileCou = 0;    
+                    @endphp
 
-            @php
-                $projInstance = $project->actual_page_instance;
-            @endphp
+                    @foreach ($projects as $projKey => $project)
 
-            <div class="col-12 col-md-6 col-lg-6 col-xl-4">
-                <div class="item">
-                    <div class="img" style="background-image: url({{ $projInstance->preview_img }})">
-                        <div class="top-bar">
-                            <div class="add" data-popup="{{ $projKey }}"><i class="far fa-plus"></i></div>
-                            
-                            @include('modules.presentation.parts.projects_tiles_popup', ['popupKey' => $projKey])
+                        @php
+                            $projInstance = $project->actual_page_instance;
+                        @endphp
+
+                        @if($tileCou === 0)
+
+                        <div class="swiper-slide">
+                            <div class="wrap">
+                                <div class="row">
+
+                        @endif
+
+                        <div class="col-12 col-md-6 col-lg-6 col-xl-4">
+                            <div class="item">
+                                <div class="img" style="background-image: url({{ $projInstance->preview_img }})">
+                                    <div class="top-bar">
+                                        <div class="add" data-popup="{{ $projKey }}"><i class="far fa-plus"></i></div>
+                                        
+                                        @include('modules.presentation.parts.projects_tiles_popup', ['popupKey' => $projKey])
+                                    </div>
+                                </div>
+                                <div class="descr">
+                                    <div class="name"><b>SPONSOR AN ORPHAN</b></div>
+                                    <div class="text-right"><a target="_blank" href="{{ url('/' . $projInstance->slug) }}" class="text-uppercase text-underline text-dark">LEARN MORE</a></div>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    <div class="descr">
-                        <div class="name"><b>SPONSOR AN ORPHAN</b></div>
-                        <div class="text-right"><a target="_blank" href="{{ url('/' . $projInstance->slug) }}" class="text-uppercase text-underline text-dark">LEARN MORE</a></div>
-                    </div>
-                </div>
-            </div>
 
-        @endforeach
+                        @php
+                            
+                            if ($tileCou < 5) {
+                                $tileCou++;
+                            } else {
+                                $tileCou = 0;
+                            }
+
+                        @endphp
+
+                        @if(($tileCou === 0) || ($projKey + 1 === count($projects)))
+                                </div>
+                            </div>
+                        </div>
+                        @endif
+
+                    @endforeach
+
+            
+        </div>
+        <div class="swiper-button-next"><i class="far fa-arrow-right"></i></div>
+        <div class="swiper-button-prev"><i class="far fa-arrow-left"></i></div>
+        <div class="swiper-pagination"></div>
+    </div>
+</div>
 
         {{-- <div class="col-12 col-md-6 col-lg-6 col-xl-4">
             <div class="item selected">
@@ -41,6 +78,4 @@
             </div>
         </div> --}}
 
-    </div>
-
-</div>
+    
