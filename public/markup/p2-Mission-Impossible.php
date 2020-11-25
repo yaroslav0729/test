57481ef9beb550441378493922509b9c835c5713
Ivan Galushka <i.galushka@greenice.net>
2020-11-25 10:18:48 +0300

    update footer markup

---
+++ public/markup/p2-Mission-Impossible.php
@@ -0,0 +1,114 @@
+<?php include "_header-short-w.php";?>
+
+<section class="head-mission-impossible bg-light">
+    <div class="wrap" style="background-image: url(img/content/head-mission-impossible.jpg)">
+        <div class="text">EMPOWER PEOPLE IN NEED</div>
+        <div class="decor-text">
+            <span class="text-red">Mission</span>
+            <span>Possible</span>
+        </div>
+    </div>
+</section>
+
+
+<div class="pt-5"></div>
+
+<section class="mission-impossible">
+    <div class="wrap">
+        <div class="title">Have you heard of...</div>
+    </div>
+    <div class="wrap">
+    <div class="body">
+        <div class="row gutter-0">
+            <div class="col-7" style="z-index: 2">
+                <div class="text bg-red">
+                    <div class="tl">Mission Possible, the mission that changes everyone's lives. </div>
+                    <p>170 Characters perspiciais und omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quaemus ab illo inventore veritatis.</p>
+                </div>
+                <div class="text-right">
+                    <a href="#" class="btn btn-danger-light view-more">Learn more</a>
+                </div>
+            </div>
+            <div class="col-5 img" style="background-image: url(img/content/mission-impossible-1.jpg)">&nbsp;</div>
+        </div>
+    </div>
+    </div>
+</section>
+
+<div class="pt-5"></div>
+
+
+
+
+<section class="discover-more bg-danger-light">
+    <div class="wrap">
+        <div class="title">
+            <div class="row">
+                <div class="col-7">
+                    <b class="font-size-30 mr-4 text-uppercase">make a difference today</b>
+                </div>
+                <div class="col-5 text-right">
+                    <a href="#" class="text-uppercase text-underline"><b>visit newsroom</b> <i class="far fa-arrow-right"></i></a>
+                </div>
+            </div>
+        </div>
+
+        <div class="current-projects-list">
+            <div class="row">
+                <div class="col-4">
+                    <a href="#" class="item">
+                        <span class="img" style="background-image: url(img/content/discover-more-1.jpg)"></span>
+                        <span class="descr">
+                    <span class="name font-size-16">EVENT</span>
+                    <span class="text font-size-16"><b>Critical campaign title, 60 char lorem sit amet, demis.</b></span>
+                </span>
+                    </a>
+                </div>
+                <div class="col-4">
+                    <a href="#" class="item">
+                        <span class="img" style="background-image: url(img/content/discover-more-2.jpg)"></span>
+                        <span class="descr">
+                    <span class="name font-size-16">PROJECT</span>
+                    <span class="text font-size-16"><b>Critical campaign title, 60 char lorem sit amet, demis.</b></span>
+                </span>
+                    </a>
+                </div>
+                <div class="col-4">
+                    <a href="#" class="item">
+                        <span class="img" style="background-image: url(img/content/discover-more-3.jpg)"></span>
+                        <span class="descr">
+                    <span class="name font-size-16">ARTICLE</span>
+                    <span class="text font-size-16"><b>Critical campaign title, 60 char lorem sit amet, demis.</b></span>
+                </span>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
+
+
+<section class="join-cause-2">
+    <div class="wrap">
+        <div>
+            <div class="row align-items-center">
+                <div class="col-7">
+                    <div class="title mb-3">
+                        <p class="font-size-30"><b>JOIN THE CAUSE</b></p>
+                    </div>
+                    <p  class="font-size-20 mb-5">There are so many ways to help, make sure you stay in the loop and <a href="#" class="text-underline text-dark">sign up</a> to our Newsletter!</p>
+                </div>
+                <div class="col-5 pr-4">
+                    <img src="img/content/join-cause-2.jpg" alt="" class="w-100">
+                    <i class="fal fa-plus decor-plus"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include "_footer.php";?>
+
+</body>
+</html>
