e7e9e32b76cc00c6358c5b10964a1ea5b3c54f37
Ivan Galushka <i.galushka@greenice.net>
2020-12-03 10:12:12 +0300

    markup mobile pahse 2:  Mission-Impossible

---
+++ public/markup/mobile/p2-Mission-Impossible.php
@@ -0,0 +1,349 @@
+<?php include "_header.php"; ?>
+
+<section class="head-mission-impossible">
+    <div class="wrap" style="background-image: url(img/content/head-mission-impossible.jpg)">
+        <div class="text">EMPOWER PEOPLE IN NEED</div>
+        <div class="decor-text">
+            <span class="text-red">Mission</span>
+            <span>Possible</span>
+        </div>
+    </div>
+</section>
+
+<section class="pt-3 pb-3 pl-4 pr-4">
+    <p class="font-size-16 mb-0"><b>THE NEXT STEP TO<br>VOLUNTEERING, MAKE IMPACTS<br>TO GLOBAL COMMUNITIES.</b></p>
+</section>
+
+<section class="swiper-mission-impossible">
+    <div class="swiper-container">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">
+                <div class="img" style="background-image: url(img/content/swiper-mission-impossible.jpg)"></div>
+            </div>
+            <div class="swiper-slide">
+                <div class="img" style="background-image: url(img/content/swiper-mission-impossible.jpg)"></div>
+            </div>
+            <div class="swiper-slide">
+                <div class="img" style="background-image: url(img/content/swiper-mission-impossible.jpg)"></div>
+            </div>
+            <div class="swiper-slide">
+                <div class="img" style="background-image: url(img/content/swiper-mission-impossible.jpg)"></div>
+            </div>
+            <div class="swiper-slide">
+                <div class="img" style="background-image: url(img/content/swiper-mission-impossible.jpg)"></div>
+            </div>
+        </div>
+    </div>
+    <script>
+        var swiper = new Swiper('.swiper-mission-impossible .swiper-container', {
+            slidesPerView: 3,
+            centeredSlides: true,
+            loop: true,
+            spaceBetween: 0
+        });
+    </script>
+</section>
+
+
+<section class="so-all-about bg-light">
+    <div class="red-line"></div>
+    <div class="title">
+        <p>So what's this  all about?</p>
+    </div>
+    <div class="bg-red p-4">
+        <p class="font-size-16 text-white mb-0">455 characters und omnis iste natus error sit voluptatem accusantim doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicab. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed conseuntur magni dolores eos qui rati voluptate sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.</p>
+    </div>
+    <img src="img/content/so-all-about.jpg" alt="" class="w-100">
+
+    <div class="help-info-swiper">
+        <div class="swiper-container">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <span>8.2k</span>
+                    <span>Meals provided</span>
+                </div>
+                <div class="swiper-slide">
+                    <span>10.1k</span>
+                    <span>Children educated</span>
+                </div>
+                <div class="swiper-slide">
+                    <span>6.6k</span>
+                    <span>People empowered</span>
+                </div>
+                <div class="swiper-slide">
+                    <span>8k</span>
+                    <span>Wells built</span>
+                </div>
+            </div>
+            <div class="swiper-button-next"><i class="far fa-arrow-right"></i></div>
+            <div class="swiper-button-prev"><i class="far fa-arrow-left"></i></div>
+        </div>
+
+        <script>
+            var swiper = new Swiper('.help-info-swiper .swiper-container', {
+                navigation: {
+                    nextEl: '.help-info-swiper .swiper-button-next',
+                    prevEl: '.help-info-swiper .swiper-button-prev',
+                },
+            });
+        </script>
+    </div>
+
+    <div class="pt-3"></div>
+    <div class="pl-4 pr-4">
+        <div class="box bg-red text-center">
+            <p class="text-white font-size-12 text-uppercase mb-4"><b>Latest mission</b></p>
+            <p class="text-white font-size-30 font-weight-light text-uppercase mb-0">Tanzania</p>
+            <p class="text-white font-size-16 text-uppercase mb-4"><b>20th August 2020</b></p>
+            <a href="#" class="btn btn-outline-primary border-white">Apply now</a>
+        </div>
+    </div>
+</section>
+
+<section class="how-does-work bg-light">
+    <div class="title">
+        <svg class="decor-wave size-25 style-danger" version="1.0" xmlns="http://www.w3.org/2000/svg" width="2202.000000pt" height="166.000000pt" viewBox="0 0 2202.000000 166.000000" preserveAspectRatio="xMidYMid meet">
+            <g transform="translate(0.000000,166.000000) scale(0.100000,-0.100000)"
+               fill="#000000" stroke="none">
+                <path d="M170 1630 c-53 -25 -92 -60 -129 -115 -23 -36 -26 -49 -26 -135 0
+    -86 3 -99 26 -135 63 -94 129 -129 263 -139 246 -18 368 -100 648 -439 168
+    -205 344 -382 451 -455 104 -71 240 -135 362 -168 94 -26 113 -28 300 -28 185
+    0 207 2 298 27 125 33 278 107 389 187 94 67 262 234 368 365 185 230 310 359
+    408 422 110 71 265 102 410 83 214 -28 326 -112 617 -465 242 -293 370 -407
+    565 -505 189 -94 285 -115 525 -115 166 1 201 4 279 24 304 79 510 233 807
+    601 51 63 147 169 213 236 182 183 281 228 491 227 129 -1 214 -22 301 -74 78
+    -47 212 -176 329 -316 55 -65 130 -155 168 -201 205 -244 435 -402 687 -470
+    87 -24 111 -26 295 -26 185 0 207 2 298 27 117 31 284 109 375 174 108 77 238
+    207 407 408 315 374 410 446 627 475 62 8 106 8 166 0 230 -31 328 -108 679
+    -536 106 -129 268 -285 366 -352 104 -72 245 -137 364 -169 91 -25 113 -27
+    298 -27 187 0 207 2 300 27 55 15 150 52 210 82 198 97 331 216 585 520 168
+    202 298 332 385 383 113 66 252 92 394 72 214 -29 327 -114 616 -465 242 -293
+    370 -407 565 -505 189 -94 285 -115 525 -115 209 1 288 14 439 76 224 93 376
+    221 639 539 231 278 377 408 501 445 96 29 198 38 293 25 213 -28 324 -112
+    620 -468 239 -288 369 -404 558 -499 177 -89 272 -113 480 -120 121 -4 182 -1
+    250 11 313 54 570 224 835 552 113 140 249 291 319 356 128 117 229 161 405
+    174 62 5 107 14 141 30 63 29 73 38 116 99 33 48 34 53 34 145 0 86 -2 99 -27
+    137 -38 59 -65 82 -127 111 -51 24 -60 24 -185 19 -179 -8 -285 -36 -451 -120
+    -191 -95 -320 -212 -560 -502 -291 -353 -403 -437 -619 -465 -94 -13 -197 -4
+    -292 25 -125 37 -258 156 -500 445 -268 322 -416 446 -640 539 -153 62 -230
+    75 -444 76 -170 0 -206 -3 -279 -23 -186 -49 -334 -127 -490 -257 -93 -78
+    -153 -142 -339 -366 -268 -324 -387 -412 -595 -439 -95 -13 -197 -4 -294 25
+    -115 34 -264 163 -457 395 -43 52 -110 132 -148 178 -206 242 -423 389 -673
+    458 -94 26 -113 28 -300 28 -185 0 -207 -2 -298 -27 -118 -31 -280 -107 -374
+    -173 -106 -75 -252 -223 -411 -414 -298 -359 -408 -441 -626 -470 -94 -13
+    -210 -1 -300 30 -127 43 -249 152 -466 415 -312 378 -519 535 -807 612 -91 25
+    -113 27 -298 27 -187 0 -206 -2 -300 -28 -122 -33 -258 -97 -362 -168 -107
+    -73 -283 -250 -451 -455 -159 -192 -276 -307 -372 -363 -143 -85 -342 -100
+    -521 -40 -132 45 -239 142 -486 439 -177 212 -270 307 -383 391 -108 80 -276
+    162 -405 197 -93 26 -113 28 -295 28 -214 -1 -292 -14 -442 -76 -224 -92 -374
+    -217 -638 -534 -336 -403 -451 -479 -715 -478 -264 2 -395 95 -745 528 -103
+    127 -237 261 -329 331 -111 83 -280 166 -406 201 -93 25 -114 27 -300 27 -185
+    0 -207 -2 -298 -27 -119 -32 -260 -97 -364 -169 -115 -79 -255 -219 -444 -444
+    -287 -343 -383 -414 -601 -444 -94 -13 -211 -1 -302 30 -124 42 -250 154 -466
+    415 -253 306 -401 438 -596 532 -152 73 -274 103 -439 109 -117 5 -134 3 -175
+    -16z"/>
+            </g>
+        </svg>
+        <p>How does it work?</p>
+        <span>Find the mission you love</span>
+    </div>
+    <div class="item">
+        <div class="num">01</div>
+        <div>FIrst you do this</div>
+        <p>160 characters und omnis iste natus error sit volupt accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi.</p>
+    </div>
+    <div class="item">
+        <div class="num">02</div>
+        <div>Then this</div>
+        <p>160 characters und omnis iste natus error sit volupt accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi.</p>
+    </div>
+    <div class="item pl-0 pl-md-5">
+        <div class="num">03</div>
+        <div>relief here we come</div>
+        <p>160 characters und omnis iste natus error sit volupt accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi.</p>
+    </div>
+</section>
+
+<section class="explore-past-missions bg-red">
+    <div class="row">
+        <div class="col-12">
+            <p class="font-size-40 text-white mb-3"><b>Explore past Missions</b></p>
+            <p class="font-size-16 text-white mb-5">210 Characters undos omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicab. </p>
+            <div class="pt-0">
+                <a href="#" class="btn btn-outline-primary border-white">Donate now</a>
+            </div>
+        </div>
+        <div class="col-12">
+            <div class="pt-5"></div>
+            <div class="swiper-container">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">
+                        <div class="item">
+                            <img src="img/content/explore-past-missions1.jpg" alt="">
+                            <span class="place"><span class="text-dark"><i class="fal fa-map-marker-alt"></i> Tanzania, africa</span></span>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="item">
+                            <img src="img/content/explore-past-missions2.jpg" alt="">
+                            <span class="place"><span class="text-dark"><i class="fal fa-map-marker-alt"></i> kashmir, India</span></span>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="item">
+                            <img src="img/content/Volunteer1.jpg" alt="">
+                            <span class="place"><span class="text-dark"><i class="fal fa-map-marker-alt"></i> TEST, TEST</span></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-pagination"></div>
+            </div>
+
+            <script>
+                var swiper = new Swiper('.explore-past-missions .swiper-container', {
+                    pagination: {
+                        el: '.explore-past-missions .swiper-pagination'
+                    }
+                });
+            </script>
+
+        </div>
+    </div>
+</section>
+
+
+<section class="experience-lifetime">
+    <p class="font-size-30 mb-0"><b>Experience of a lifetime</b></p>
+    <p class="font-size-16 mb-5 text-danger">Hear from past Mission Possible Volunteers</p>
+    <p class="font-size-40 mb-1 text-danger">JAN 20TH</p>
+    <p class="font-size-16 mb-3"><b>FATMA ALANI'S STORY</b></p>
+    <div class="pr-0 pr-md-5">
+        <p class="font-size-16 mb-5">"160 characters und omnis iste natus error sit volupt accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi."</p>
+    </div>
+
+    <div class="img-video" style="background-image: url(img/content/experience-lifetime.jpg)"><i class="fas fa-play-circle"></i></div>
+</section>
+
+
+<section class="be-part-possible bg-red">
+    <div class="">
+        <p class="font-size-30 text-white mb-3"><b>Be part of the Possible</b></p>
+        <p class="font-size-16 text-white mb-5">210 Characters undos omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicab. </p>
+        <div class="pt-0">
+            <a href="#" class="btn btn-outline-primary border-white">Apply now</a>
+        </div>
+    </div>
+</section>
+
+<section class="our-latest-mission">
+    <div class="title">Our latest Mission</div>
+    <div class="bg-blue">
+        <div class="text">
+            <p class="text-white font-size-12 mb-3 text-uppercase"><b>Deployments</b></p>
+            <div class="tl text-white">Tanzania</div>
+            <p class="text-white font-size-16 mb-4 text-uppercase"><b>20th August 2020</b></p>
+            <svg class="decor-wave size-25 style-white opacity-20 mb-4" version="1.0" xmlns="http://www.w3.org/2000/svg" width="2202.000000pt" height="166.000000pt" viewBox="0 0 2202.000000 166.000000" preserveAspectRatio="xMidYMid meet">
+                <g transform="translate(0.000000,166.000000) scale(0.100000,-0.100000)"
+                   fill="#000000" stroke="none">
+                    <path d="M170 1630 c-53 -25 -92 -60 -129 -115 -23 -36 -26 -49 -26 -135 0
+    -86 3 -99 26 -135 63 -94 129 -129 263 -139 246 -18 368 -100 648 -439 168
+    -205 344 -382 451 -455 104 -71 240 -135 362 -168 94 -26 113 -28 300 -28 185
+    0 207 2 298 27 125 33 278 107 389 187 94 67 262 234 368 365 185 230 310 359
+    408 422 110 71 265 102 410 83 214 -28 326 -112 617 -465 242 -293 370 -407
+    565 -505 189 -94 285 -115 525 -115 166 1 201 4 279 24 304 79 510 233 807
+    601 51 63 147 169 213 236 182 183 281 228 491 227 129 -1 214 -22 301 -74 78
+    -47 212 -176 329 -316 55 -65 130 -155 168 -201 205 -244 435 -402 687 -470
+    87 -24 111 -26 295 -26 185 0 207 2 298 27 117 31 284 109 375 174 108 77 238
+    207 407 408 315 374 410 446 627 475 62 8 106 8 166 0 230 -31 328 -108 679
+    -536 106 -129 268 -285 366 -352 104 -72 245 -137 364 -169 91 -25 113 -27
+    298 -27 187 0 207 2 300 27 55 15 150 52 210 82 198 97 331 216 585 520 168
+    202 298 332 385 383 113 66 252 92 394 72 214 -29 327 -114 616 -465 242 -293
+    370 -407 565 -505 189 -94 285 -115 525 -115 209 1 288 14 439 76 224 93 376
+    221 639 539 231 278 377 408 501 445 96 29 198 38 293 25 213 -28 324 -112
+    620 -468 239 -288 369 -404 558 -499 177 -89 272 -113 480 -120 121 -4 182 -1
+    250 11 313 54 570 224 835 552 113 140 249 291 319 356 128 117 229 161 405
+    174 62 5 107 14 141 30 63 29 73 38 116 99 33 48 34 53 34 145 0 86 -2 99 -27
+    137 -38 59 -65 82 -127 111 -51 24 -60 24 -185 19 -179 -8 -285 -36 -451 -120
+    -191 -95 -320 -212 -560 -502 -291 -353 -403 -437 -619 -465 -94 -13 -197 -4
+    -292 25 -125 37 -258 156 -500 445 -268 322 -416 446 -640 539 -153 62 -230
+    75 -444 76 -170 0 -206 -3 -279 -23 -186 -49 -334 -127 -490 -257 -93 -78
+    -153 -142 -339 -366 -268 -324 -387 -412 -595 -439 -95 -13 -197 -4 -294 25
+    -115 34 -264 163 -457 395 -43 52 -110 132 -148 178 -206 242 -423 389 -673
+    458 -94 26 -113 28 -300 28 -185 0 -207 -2 -298 -27 -118 -31 -280 -107 -374
+    -173 -106 -75 -252 -223 -411 -414 -298 -359 -408 -441 -626 -470 -94 -13
+    -210 -1 -300 30 -127 43 -249 152 -466 415 -312 378 -519 535 -807 612 -91 25
+    -113 27 -298 27 -187 0 -206 -2 -300 -28 -122 -33 -258 -97 -362 -168 -107
+    -73 -283 -250 -451 -455 -159 -192 -276 -307 -372 -363 -143 -85 -342 -100
+    -521 -40 -132 45 -239 142 -486 439 -177 212 -270 307 -383 391 -108 80 -276
+    162 -405 197 -93 26 -113 28 -295 28 -214 -1 -292 -14 -442 -76 -224 -92 -374
+    -217 -638 -534 -336 -403 -451 -479 -715 -478 -264 2 -395 95 -745 528 -103
+    127 -237 261 -329 331 -111 83 -280 166 -406 201 -93 25 -114 27 -300 27 -185
+    0 -207 -2 -298 -27 -119 -32 -260 -97 -364 -169 -115 -79 -255 -219 -444 -444
+    -287 -343 -383 -414 -601 -444 -94 -13 -211 -1 -302 30 -124 42 -250 154 -466
+    415 -253 306 -401 438 -596 532 -152 73 -274 103 -439 109 -117 5 -134 3 -175
+    -16z"/>
+                </g>
+            </svg>
+            <p class="text-white font-size-16">170 Characters perspiciais und omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quaemus ab illo inventore veritatis.</p>
+        </div>
+    </div>
+    <div class="img" style="background-image: url(img/content/mission-impossible-1.jpg)">
+        <a href="#" class="btn btn-blue view-more">Apply now</a>
+    </div>
+</section>
+
+<section class="discover-more bg-danger-light">
+    <div class="wrap">
+        <div class="title">
+            <b class="font-size-25 text-uppercase">Related topics</b>
+        </div>
+        <div class="current-projects-list current-projects-swiper">
+            <div class="swiper-container">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">
+                        <a href="#" class="item">
+                            <span class="img" style="background-image: url(img/content/discover-more-1.jpg)"></span>
+                            <span class="descr">
+                            <span class="name font-size-16">EVENT</span>
+                            <span class="text font-size-16"><b>Critical campaign title, 60 char lorem sit amet, demis.</b></span></span>
+                        </a>
+                    </div>
+                    <div class="swiper-slide">
+                        <a href="#" class="item">
+                            <span class="img" style="background-image: url(img/content/discover-more-2.jpg)"></span>
+                            <span class="descr">
+                                <span class="name font-size-16">PROJECT</span>
+                                <span class="text font-size-16"><b>Critical campaign title, 60 char lorem sit amet, demis.</b></span>
+                            </span>
+                        </a>
+                    </div>
+                    <div class="swiper-slide">
+                        <a href="#" class="item">
+                            <span class="img" style="background-image: url(img/content/discover-more-3.jpg)"></span>
+                            <span class="descr">
+                            <span class="name font-size-16">ARTICLE</span>
+                            <span class="text font-size-16"><b>Critical campaign title, 60 char lorem sit amet, demis.</b></span>
+                        </span>
+                        </a>
+                    </div>
+                </div>
+                <div class="swiper-pagination"></div>
+            </div>
+        </div>
+
+        <script>
+            var swiper = new Swiper('.current-projects-swiper .swiper-container', {
+                pagination: {
+                    el: '.current-projects-swiper .swiper-pagination'
+                }
+            });
+        </script>
+
+
+    </div>
+</section>
+
+<?php include "_footer.php";?>
+
+</body>
+</html>
