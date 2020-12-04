4f35b26ab633ddb6a3d88aaa60493323086dab3d
Kirill Vorotilov <kirill.vorotilov@greenice.net>
2020-12-04 17:37:30 +0300

    Task scheduler - clear cart items

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,6 +16,11 @@ class Kernel extends ConsoleKernel
         //
     ];
 
+    protected function scheduleTimezone()
+    {
+        return 'Europe/London';
+    }
+
     /**
      * Define the application's command schedule.
      *
@@ -24,7 +29,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('clear:cart')->dailyAt('00:00');
     }
 
     /**
