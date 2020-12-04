124f58c807faec95f355a0a361c42be89428d42f
Ivan Galushka <i.galushka@greenice.net>
2020-12-04 12:59:40 +0300

    markup mobile phase 2: Newsroom

---
+++ public/markup/mobile/p2-Newsroom.php
@@ -0,0 +1,206 @@
+<?php include "_header.php"; ?>
+
+<section class="newsroom-tabs">
+    <nav class="general-content-tabs">
+        <div class="nav nav-tabs nav-fill"  role="tablist">
+            <a class="nav-link active"  href="#" >TRENDING</a>
+            <a class="nav-link "  href="#" >NEWS</a>
+            <a class="nav-link "  href="#" >PRESS</a>
+            <a class="nav-link "  href="#" >IN CINEMA</a>
+        </div>
+    </nav>
+</section>
+
+<section class="blog-article-head">
+    <div class="wrap no-brd pt-0">
+        <div class="article-text">
+            <div class="img-video" style="background-image: url(img/content/blog-video-1.jpg)"><i class="fas fa-play-circle"></i></div>
+            <div class="article-text">
+                <p class="font-size-12 mb-0"><b>Featured article</b></p>
+                <h1 class="mb-3  pb-0">Article title right here, lorem ipsum 90ch.</h1>
+                <div class="date"><span class="text-danger">7mins</span></div>
+                <p>460 Characters perspiciais und omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis.</p>
+                <div>
+                    <a href="#" class="btn btn-red">Watch now</a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
+
+<section class="newsroom-list" >
+    <div class="title">
+        <div>
+            <span>Trending</span>
+            <i>Trending articles</i>
+        </div>
+        <div>
+            <button class="btn btn-white">SORT BY DATE</button>
+            <button class="btn btn-primary-dark">FILTER BY TOPIC</button>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-12">
+            <div class="item">
+                <div>
+                    <a href="#" class="tl">Article title placement to go right here 50ch max</a>
+                    <div class="date">
+                        <div><b>4min read</b></div>
+                        APRIL 06<span>•</span>BY AHMED SALEM
+                    </div>
+                </div>
+                <a href="#" class="img" style="background-image: url(img/content/explore-past-missions1.jpg)"></a>
+            </div>
+        </div>
+        <div class="col-12">
+            <div class="item">
+                <div>
+                    <a href="#" class="tl">Article title placement to go right here 50ch max</a>
+                    <div class="date">
+                        <div><b>4min read</b></div>
+                        APRIL 06<span>•</span>BY AHMED SALEM
+                    </div>
+                </div>
+                <a href="#" class="img" style="background-image: url(img/content/explore-past-missions1.jpg)"></a>
+            </div>
+        </div>
+        <div class="col-12">
+            <div class="item">
+                <div>
+                    <a href="#" class="tl">Article title placement to go right here 50ch max</a>
+                    <div class="date">
+                        <div><b>4min read</b></div>
+                        APRIL 06<span>•</span>BY AHMED SALEM
+                    </div>
+                </div>
+                <a href="#" class="img" style="background-image: url(img/content/explore-past-missions1.jpg)"></a>
+            </div>
+        </div>
+        <div class="col-12">
+            <div class="item">
+                <div>
+                    <a href="#" class="tl">Article title placement to go right here 50ch max</a>
+                    <div class="date">
+                        <div><b>4min read</b></div>
+                        APRIL 06<span>•</span>BY AHMED SALEM
+                    </div>
+                </div>
+                <a href="#" class="img" style="background-image: url(img/content/explore-past-missions1.jpg)"></a>
+            </div>
+        </div>
+    </div>
+
+    <div class="pt-3 pb-3"></div>
+
+    <ul class="pagination justify-content-center">
+        <li class="page-item">
+            <a class="page-link arrow" href="#" aria-label="Previous">
+                <i class="far fa-chevron-left"></i>
+            </a>
+        </li>
+        <li class="page-item active"><a class="page-link" href="#">1</a></li>
+        <li class="page-item"><a class="page-link" href="#">2</a></li>
+        <li class="page-item"><a class="page-link" href="#">3</a></li>
+        <li class="page-item">
+            <a class="page-link arrow" href="#" aria-label="Next">
+                <i class="far fa-chevron-right"></i>
+            </a>
+        </li>
+    </ul>
+</section>
+
+
+<section class="popular-topic-list">
+    <div class="title">
+        <b>POPULAR TOPICS</b>
+    </div>
+        <div class="current-projects-list current-projects-swiper">
+            <div class="swiper-container">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">
+                        <a href="#" class="item">
+                            <span class="img" style="background-image: url(img/content/popular-topic-list1.jpg)"></span>
+                            <span class="descr">
+                                <span class="name font-size-16"><b>CHARITY</b></span>
+                                <span class="num">01</span>
+                                <span class="text font-size-16 mb-3"><b>Article title placement here with a maximum of 60 characters.</b></span>
+                                <span class="date">April 06, 2020 BY AHMED SALEM</span>
+                                <i class="far fa-plus"></i>
+                            </span>
+                        </a>
+                    </div>
+                    <div class="swiper-slide">
+                        <a href="#" class="item">
+                            <span class="img" style="background-image: url(img/content/popular-topic-list2.jpg)"></span>
+                            <span class="descr">
+                                <span class="name font-size-16"><b>EVENT</b></span>
+                                <span class="num">02</span>
+                                <span class="text font-size-16 mb-3"><b>Article title placement here with a maximum of 60 characters.</b></span>
+                                <span class="date">April 06, 2020 BY AHMED SALEM</span>
+                                <i class="far fa-plus"></i>
+                            </span>
+                        </a>
+                    </div>
+                    <div class="swiper-slide">
+                        <a href="#" class="item">
+                            <span class="img" style="background-image: url(img/content/popular-topic-list3.jpg)"></span>
+                            <span class="descr">
+                                <span class="name font-size-16"><b>PROJECT</b></span>
+                                <span class="num">03</span>
+                                <span class="text font-size-16 mb-3"><b>Article title placement here with a maximum of 60 characters.</b></span>
+                                <span class="date">April 06, 2020 BY AHMED SALEM</span>
+                                <i class="far fa-plus"></i>
+                            </span>
+                        </a>
+                    </div>
+                </div>
+                <div class="swiper-pagination"></div>
+            </div>
+        </div>
+
+        <script>
+            var swiper = new Swiper('.current-projects-swiper .swiper-container', {
+                pagination: {
+                    el: '.current-projects-swiper .swiper-pagination'
+                }
+            });
+        </script>
+</section>
+
+<section class="mission-impossible">
+    <div class="title no-brd">Mission Impossible</div>
+    <div class="wrap">
+        <div class="body">
+            <div class="text bg-danger">
+                <div class="tl">Applications for MP 2020 deployments are open!</div>
+            </div>
+            <div class="img" style="background-image: url(img/content/project-2.jpg)">&nbsp;</div>
+            <div class="text-center bg-danger-light">
+                <a href="#" class="btn btn-danger-light view-more">Learn more</a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="join-cause pb-0 with-glyph">
+    <div class="wrap">
+        <div class="title text-center">
+            <p class="font-size-25"><b>Join the cause!</b></p>
+        </div>
+        <p class="font-size-20 mb-4  text-center">
+            There are so many ways to help, stay in the loop with our Newsletter.
+        </p>
+        <form action="/" class="d-flex">
+            <input type="text" placeholder="Your email address" class="flex-grow-1">
+            <button type="submit"><i class="far fa-chevron-right"></i></button>
+        </form>
+        <div class="pt-4"></div>
+    </div>
+</section>
+
+<?php include "_footer.php";?>
+
+</body>
+</html>
