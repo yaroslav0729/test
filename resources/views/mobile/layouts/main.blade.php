60ff7292742eb786e4f230f7ab38e03551ecca7e
Kirill Vorotilov <kirill.vorotilov@greenice.net>
2020-12-09 09:58:26 +0300

    donation module - add to cart - mobile version

--- resources/views/mobile/layouts/main.blade.php
+++ resources/views/mobile/layouts/main.blade.php
@@ -23,7 +23,7 @@
                     <div class="col-6"><a href="#" class="logo"><span></span> Islamic Help</a></div>
                     <div class="col-6 text-right">
                         <a href="{{ url('/donate#about-donation') }}" class="basket"><i class=""></i>
-                            @if($cartSum !== 0) <span></span> @endif
+                            <span class="@if($cartSum === 0) d-none @endif"></span>
                         </a>
                         <span class="open-head-menu"></span>
                     </div>
