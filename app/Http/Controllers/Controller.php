0434cdb85ade05dd449bbc6acc3faa6f03f71b8d
Kirill Vorotilov <kirill.vorotilov@greenice.net>
2020-12-10 16:39:11 +0300

    Send thank you template, save Email log - testing

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,13 +8,27 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Carbon;
 
+use Illuminate\Support\Facades\Mail;
+use App\Models\Order;
+use App\Mail\ThankYouDonation;
+use App\Models\EmailLog;
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests; 
 
     public function test()
     {
-        dd('ok');
+        $order = Order::findOrFail(90);
+
+        $emailTo = $order->email;
+        $subject = 'Thank you for donation';
+        $emailFrom = env('MAIL_FROM_ADDRESS');
+
+        Mail::to($emailTo)
+            ->send(new ThankYouDonation($order, $subject, $emailTo, $emailFrom));
+
+        dd('email send ok');
     }
 
     
--- app/Models/EmailLog.php
+++ app/Models/EmailLog.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class EmailLog extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'email_to', 'subject', 'body'
+    ];
 }
