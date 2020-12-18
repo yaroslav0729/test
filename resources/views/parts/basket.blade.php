ca550951cb06b02ee4bf23b4dddcab852a3e993d
Ivan Galushka <i.galushka@greenice.net>
2020-12-18 16:40:21 +0300

    update

--- resources/views/parts/basket.blade.php
+++ resources/views/parts/basket.blade.php
@@ -2,6 +2,6 @@
     $cartSum = \App\Models\CartItem::getCartSum(); 
 @endphp
 
-<div class="basket" data-toggle="modal" data-target="#cartModal"><i></i> 
+<div class="basket" data-toggle="modal" data-target="#cartModal"><i></i>
     <span class="@if($cartSum === 0) d-none @endif">£<object id="sum" >{{ $cartSum }}</object></span>
 </div>
