53b2ad8eabc00d89d279ec4bbda437761aa0df57
vetal33 <v.bitko@gmail.com>
2020-12-23 16:59:52 +0200

    remove always shake cart when not empty

--- resources/views/parts/basket.blade.php
+++ resources/views/parts/basket.blade.php
@@ -1,7 +1,7 @@
 @php
-    $cartSum = \App\Models\CartItem::getCartSum(); 
+    $cartSum = \App\Models\CartItem::getCartSum();
 @endphp
 
-<div class="basket @if($cartSum !== 0) bell-animate @endif" data-toggle="modal" data-target="#cartModal"><i></i>
+<div class="basket" data-toggle="modal" data-target="#cartModal"><i></i>
     <span class="pulse @if($cartSum === 0) d-none @endif">£<object id="sum" >{{ $cartSum }}</object></span>
 </div>
