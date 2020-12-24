9ff321bc013524b97b5fcfaeda8d958f5fd3aa42
vetal33 <v.bitko@gmail.com>
2020-12-24 14:12:18 +0200

    fix form for mobile

--- resources/views/mobile/modules/presentation/join_the_cause_subscribe2.blade.php
+++ resources/views/mobile/modules/presentation/join_the_cause_subscribe2.blade.php
@@ -85,6 +85,7 @@
             </svg>
         </div>
         <form id="subscription_form" action="{{ route('subscribe') }}" method="POST" class="d-flex">
+            @csrf
             <input type="text" placeholder="Your email address" class="flex-grow-1">
             <button type="submit" id="subscription_sbmt"><i class="far fa-chevron-right"></i></button>
         </form>
