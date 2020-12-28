9d7daf55556229f802dd30e52ca3a9e9d7863cd4
Kirill Vorotilov <kirill.vorotilov@greenice.net>
2020-12-28 16:00:37 +0300

    fix category name - length validation

--- app/Http/Requests/Admin/CreateEditCategoryRequest.php
+++ app/Http/Requests/Admin/CreateEditCategoryRequest.php
@@ -24,8 +24,8 @@ class CreateEditCategoryRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required',
-            'slug' => 'required',
+            'name' => 'required|max:100',
+            'slug' => 'required|max:255',
         ];
     }
 }
