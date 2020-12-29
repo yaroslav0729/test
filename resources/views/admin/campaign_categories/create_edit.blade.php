4e25512c50f099a761a4f05cad4349fb96f04897
Kirill Vorotilov <kirill.vorotilov@greenice.net>
2020-12-29 09:39:29 +0300

    admin styles

--- resources/views/admin/campaign_categories/create_edit.blade.php
+++ resources/views/admin/campaign_categories/create_edit.blade.php
@@ -12,7 +12,7 @@
 
 @section('content')
 
-<div id="admin_content" class="bg-gray-100 flex-auto h-screen">
+<div id="admin_content" class="bg-gray-100 flex-auto">
     <div class="p-5 pb-8 lg:w-1/2">
         <h1>{{ $pageTitle }}</h1>
 
