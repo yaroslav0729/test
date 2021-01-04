9a4d68fa311000a6b128f789119d2370f6660887
Kirill Vorotilov <kirill.vorotilov@greenice.net>
2021-01-04 15:11:03 +0300

    Proj page - heading 30 charters

--- app/Models/Template.php
+++ app/Models/Template.php
@@ -368,6 +368,7 @@ class Template
             case self::PROJECT_PAGE: {
 
                 $rules = [
+                    'parameters.proj_heading' => 'max:30',
                     'parameters.amount' => 'amount_text',
                     'parameters.donate_text' => 'max:100',
                     'parameters.what_happens_text' => 'max:180',
