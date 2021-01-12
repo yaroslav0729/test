70b39a6d9e0f7edebd392140e56c5a420ffea668
Ivan Galushka <i.galushka@greenice.net>
2021-01-12 10:46:30 +0300

    update

--- public/js/plugins/link/plugin.js
+++ public/js/plugins/link/plugin.js
@@ -1575,7 +1575,7 @@ function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "functi
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /var/www/html/islamichelp.local/node_modules/tinymce/plugins/link/plugin.js */"./node_modules/tinymce/plugins/link/plugin.js");
+module.exports = __webpack_require__(/*! /Users/ivansusanin/Documents/work/www/Islamic-Help.lo/node_modules/tinymce/plugins/link/plugin.js */"./node_modules/tinymce/plugins/link/plugin.js");
 
 
 /***/ })
