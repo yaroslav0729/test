0389c77ea6e7bb3cfc819341260b3589a41835f8
Kirill Vorotilov <kirill.vorotilov@greenice.net>
2021-01-18 15:22:09 +0300

    Login with facebook - Socialite

--- config/services.php
+++ config/services.php
@@ -30,4 +30,10 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'facebook' => [
+        'client_id' => env('FACEBOOK_KEY'),
+        'client_secret' => env('FACEBOOK_SECRET'),
+        'redirect' => env('FACEBOOK_REDIRECT_URI')
+    ],
+
 ];
