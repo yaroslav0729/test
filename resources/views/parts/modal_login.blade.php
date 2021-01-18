cad93c1287c3c6278235ffbfd7a0b7c66ee12e60
Kirill Vorotilov <kirill.vorotilov@greenice.net>
2021-01-18 09:55:33 +0300

    Login modal - Forgot password integrated

--- resources/views/parts/modal_login.blade.php
+++ resources/views/parts/modal_login.blade.php
@@ -55,14 +55,29 @@
                             <span>Login</span>
                         </button>
                     </div>
+                </form>
 
+                @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                @endif
+
+                <form method="POST" action="{{ route('password.email') }}">
+                    @csrf
                     <div class="forgot-pass" style="display: none">
                         <div class="line"></div>
                         <div class="row">
                             <div class="col-12 col-md-8">
                                 <div class="form-group">
                                     <label>Forgot password? <sup>*</sup></label>
-                                    <input type="text" class="form-control" placeholder="example@islamichelp.org.uk">
+                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="example@islamichelp.org.uk">
+                                    
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
                                 </div>
                             </div>
                             <div class="col-12 col-md-4 pt-4">
