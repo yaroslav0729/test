a100aa8bdf7bf6f2d737d93109371f52b8deb457
Kirill Vorotilov <kirill.vorotilov@greenice.net>
2021-01-21 16:24:06 +0300

    Global pay - test update status

--- app/Http/Controllers/GlobalPayController.php
+++ app/Http/Controllers/GlobalPayController.php
@@ -6,11 +6,22 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Log;
+use App\Models\Page;
+use App\Models\Template;
 
 class GlobalPayController extends Controller
 {
     public function result(Request $request)
     {
-        dd($request->all());
+        Log::info('Global pay request: '. $request);
+
+        $thanksUrl = Page::getSinglePageUrl(Template::THANK_YOU_DONATE_PAGE);
+
+        if ($thanksUrl === url('/')) {
+            die('Page with template "' . Template::getLabel(Template::THANK_YOU_DONATE_PAGE) . '" is not found.');
+        }
+
+        return redirect($thanksUrl);
     }
 }
