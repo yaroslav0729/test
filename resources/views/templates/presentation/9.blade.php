c57ae6792501c0d9d9aef917dae5fcc2b5890dba
Ivan Galushka <i.galushka@greenice.net>
2021-01-25 11:47:15 +0300

    update

--- resources/views/templates/presentation/9.blade.php
+++ resources/views/templates/presentation/9.blade.php
@@ -34,7 +34,7 @@
         <div class="text">
             <div>{!! \App\Helpers\StrHelper::addSpanWithClass($donationText, 'text-dark-blue') !!}</div>
             <p>You're awesome.</p>
-            <svg class="decor-wave style-red" version="1.0" xmlns="http://www.w3.org/2000/svg" width="2202.000000pt" height="166.000000pt" viewBox="0 0 2202.000000 166.000000" preserveAspectRatio="xMidYMid meet">
+            <svg class="decor-wave style-red size-15" version="1.0" xmlns="http://www.w3.org/2000/svg" width="2202.000000pt" height="166.000000pt" viewBox="0 0 2202.000000 166.000000" preserveAspectRatio="xMidYMid meet">
                 <g transform="translate(0.000000,166.000000) scale(0.100000,-0.100000)"
                    fill="#000000" stroke="none">
                     <path d="M170 1630 c-53 -25 -92 -60 -129 -115 -23 -36 -26 -49 -26 -135 0
