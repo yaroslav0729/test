85a1bc3c44e1aaf60d8f9e23a140ed238647a62f
Kirill Vorotilov <kirill.vorotilov@greenice.net>
2021-01-26 15:28:45 +0300

    Donations parser - bind donation to user

--- app/Console/Commands/ConvertDonationHistory.php
+++ app/Console/Commands/ConvertDonationHistory.php
@@ -12,6 +12,7 @@ use \App\Models\CampaignPrice;
 use App\Models\CampaignCategory;
 use App\Models\Donation;
 use App\Models\Order;
+use App\Models\User;
 
 class ConvertDonationHistory extends Command
 {
@@ -65,16 +66,31 @@ class ConvertDonationHistory extends Command
                         ->select('donate_items.*', 'donate_orders.*', 'donate_items.order_id as order_id', 'donate_orders.order_id as d_order_id' )
                         ->get();
 
+        $cou = count($donations);
+
         foreach ($donations as $donationKey => $donation) {
             $copyDonation = Donation::where('wp_id', $donation->id)->first();
 
+            $userId = null;
+            $donationUser = User::where('wp_id', $donation->user)->first();
+            if ($donationUser) {
+                $userId = $donationUser->id;    
+            }
+
+            $amount = $donation->amount;
+
+            if ($amount > 1000000) {
+                $amount = 0; // fix an error in WP database amount 1e38
+            }
+
             if ($copyDonation) {
                 $copyDonation->update([
-                    'value' => $donation->amount,
+                    'value' => $amount,
                     'status' => $donation->status,
                     'type' => $donation->period === 0 ? CampaignPrice::TYPE_SINGLE : CampaignPrice::TYPE_MONTHLY,
                     'email' => $donation->email,
                     'campaign_id' => null,
+                    'user_id' => $userId,
                     'currency' => 'GBP',
                     'campaign_category_id' => $this->getCampaignCategoryId($donation->type),
                     'note' => $donation->message,
@@ -83,11 +99,12 @@ class ConvertDonationHistory extends Command
                 $infoString = $donationKey + 1 . ': Donation updated => id: ' . $copyDonation->id;
             } else {
                 $copyDonation = Donation::create([
-                    'value' => $donation->amount,
+                    'value' => $amount,
                     'status' => $donation->status,
                     'type' => $donation->period === 0 ? CampaignPrice::TYPE_SINGLE : CampaignPrice::TYPE_MONTHLY,
                     'email' => $donation->email,
                     'currency' => 'GBP',
+                    'user_id' => $userId,
                     'campaign_category_id' => $this->getCampaignCategoryId($donation->type),
                     'note' => $donation->message,
                     'wp_id' => $donation->id,
@@ -124,7 +141,7 @@ class ConvertDonationHistory extends Command
                     'wp_id' => $donation->order_id, 
                 ]);
 
-                $infoString = $donationKey + 1 . ': Order updated => id: ' . $copyOrder->id;
+                $infoString = $donationKey + 1 . ' from ' . $cou . ': Order updated => id: ' . $copyOrder->id;
             } else {
                 $copyOrder = Order::create([
                     'title' => $donation->title,
@@ -148,7 +165,7 @@ class ConvertDonationHistory extends Command
                     'wp_id' => $donation->order_id,
                 ]);
 
-                $infoString = $donationKey + 1 . ': Order created => id: ' . $copyOrder->id;
+                $infoString = $donationKey + 1 . ' from ' . $cou . ': Order created => id: ' . $copyOrder->id;
             }
 
             $copyDonation->update([
