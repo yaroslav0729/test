a4b3b5d367b7548a8ef07f4eba8c44248dde35fe
Kirill Vorotilov <kirill.vorotilov@greenice.net>
2021-02-02 17:23:04 +0300

    Project - separate 3 fields of text & headers

--- resources/views/templates/form/6.blade.php
+++ resources/views/templates/form/6.blade.php
@@ -1,15 +1,47 @@
 @php
 
-    $mainHtml = "";
-
-    if (isset($parameters['main_html'])) {
-        $mainHtml = $parameters['main_html'];    
-    }
-
     $projHeading = "";
+
+    $projPar1 = "";
+    $projPar2 = "";
+    $projPar3 = "";
+
+    $projVideo = "";
+
+    $projHeader1 = "";
+    $projHeader2 = "";
+    $projHeader3 = "";
 
     if (isset($parameters['proj_heading'])) {
         $projHeading = $parameters['proj_heading'];    
+    }
+
+    if (isset($parameters['proj_par1'])) {
+        $projPar1 = $parameters['proj_par1'];    
+    }
+
+    if (isset($parameters['proj_par2'])) {
+        $projPar2 = $parameters['proj_par2'];    
+    }
+
+    if (isset($parameters['proj_par3'])) {
+        $projPar3 = $parameters['proj_par3'];    
+    }
+
+    if (isset($parameters['proj_hdr1'])) {
+        $projHeader1 = $parameters['proj_hdr1'];    
+    }
+
+    if (isset($parameters['proj_hdr2'])) {
+        $projHeader2 = $parameters['proj_hdr2'];    
+    }
+
+    if (isset($parameters['proj_hdr3'])) {
+        $projHeader3 = $parameters['proj_hdr3'];    
+    }
+
+    if (isset($parameters['proj_video'])) {
+        $proVideo = $parameters['proj_video'];    
     }
 
 @endphp
@@ -20,8 +52,38 @@
 </div>
 
 <div class="form-group">
-    <label>Main page content</label>
-    <textarea wysiwyg-editor id="main_page" class="form-control" name="parameters[main_html]" placeholder="Insert main page content text">{{ $mainHtml }}</textarea>
+    <label>Header 1:</label>
+    <input class="form-control" name="parameters[proj_hdr1]" placeholder="Insert text" value="{{ $projHeader1 }}" />
+</div>
+
+<div class="form-group">
+    <label>Paragraph 1:</label>
+    <textarea rows="3" class="form-control" name="parameters[proj_par1]" placeholder="Insert text">{{ $projPar1 }}</textarea>
+</div>
+
+<div class="form-group">
+    <label>Header 2:</label>
+    <input class="form-control" name="parameters[proj_hdr2]" placeholder="Insert text" value="{{ $projHeader2 }}" />
+</div>
+
+<div class="form-group">
+    <label>Paragraph 2:</label>
+    <textarea rows="3" class="form-control" name="parameters[proj_par2]" placeholder="Insert text">{{ $projPar2 }}</textarea>
+</div>
+
+<div class="form-group">
+    <label>Video id:</label>
+    <input class="form-control" name="parameters[proj_video]" placeholder="Insert video id" value="{{ $projVideo }}" />
+</div>
+
+<div class="form-group">
+    <label>Header 3:</label>
+    <input class="form-control" name="parameters[proj_hdr3]" placeholder="Insert text" value="{{ $projHeader3 }}" />
+</div>
+
+<div class="form-group">
+    <label>Paragraph 3:</label>
+    <textarea rows="3" class="form-control" name="parameters[proj_par3]" placeholder="Insert text">{{ $projPar3 }}</textarea>
 </div>
 
 @include('modules.admin.donate_module')
