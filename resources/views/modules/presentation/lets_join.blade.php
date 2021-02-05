3dccb50957cda221e788355faf0d3ef9adefd6d4
Ivan Galushka <i.galushka@greenice.net>
2021-02-05 10:52:41 +0300

    update

--- resources/views/modules/presentation/lets_join.blade.php
+++ resources/views/modules/presentation/lets_join.blade.php
@@ -99,11 +99,13 @@
                             <a href="{{ $link1 }}"><div class="img" style="background-image: url({{ $img1 }})"></div></a>
                         </div>
                         @else
-                        <div class="col-12 col-lg-6 img-video videoWrapper">
-                            <iframe width="400" height="300" src="https://www.youtube.com/embed/{{ $video1 }}"
-                                frameborder="0"
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                allowfullscreen></iframe>
+                        <div class="col-12 col-lg-6">
+                            <div class="img-video videoWrapper">
+                                <iframe width="400" height="300" src="https://www.youtube.com/embed/{{ $video1 }}"
+                                        frameborder="0"
+                                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                        allowfullscreen></iframe>
+                            </div>
                         </div>
                         @endempty
 
@@ -132,11 +134,13 @@
                             <a href="{{ $link2 }}"><div class="img" style="background-image: url({{ $img2 }})"></div></a>
                         </div>
                         @else
-                        <div class="col-12 col-lg-6 img-video videoWrapper">
-                            <iframe width="400" height="300" src="https://www.youtube.com/embed/{{ $video2 }}"
-                                frameborder="0"
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                allowfullscreen></iframe>
+                        <div class="col-12 col-lg-6">
+                            <div class="img-video videoWrapper">
+                                <iframe width="400" height="300" src="https://www.youtube.com/embed/{{ $video2 }}"
+                                    frameborder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                    allowfullscreen></iframe>
+                            </div>
                         </div>
                         @endempty
                         <div class="col-12 col-lg-6">
