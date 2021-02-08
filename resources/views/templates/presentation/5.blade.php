8ab13895eaa65d51b93c4d2b1ad54bf0381ac575
vetal33 <vitalii.bytko@greenice.net>
2021-02-08 19:02:10 +0200

    remove arrows from slider Donate page

--- resources/views/templates/presentation/5.blade.php
+++ resources/views/templates/presentation/5.blade.php
@@ -3,25 +3,25 @@
     $mainTitle = "";
 
     if (isset($parameters['main_title'])) {
-        $mainTitle = $parameters['main_title'];    
+        $mainTitle = $parameters['main_title'];
     }
 
     $afterTitleText = "";
 
     if (isset($parameters['after_text'])) {
-        $afterTitleText = $parameters['after_text'];    
+        $afterTitleText = $parameters['after_text'];
     }
 
     $donateToProjTitle = "";
 
     if (isset($parameters['donate_to_title'])) {
-        $donateToProjTitle = $parameters['donate_to_title'];    
+        $donateToProjTitle = $parameters['donate_to_title'];
     }
 
     $donateToProjText = "";
 
     if (isset($parameters['donate_to_text'])) {
-        $donateToProjText = $parameters['donate_to_text'];    
+        $donateToProjText = $parameters['donate_to_text'];
     }
 
     $sProjects = \App\Models\Project::getSingleProjects();
@@ -53,7 +53,7 @@
                     @else
                         <p class="font-size-16">{{ $afterTitleText }}</p>
                     @endempty
-                    
+
                 </div>
             </div>
         </div>
@@ -82,7 +82,6 @@
                     @else
                         <p class="font-size-16">{{ $donateToProjText }}</p>
                     @endempty
-                    
 
                 </div>
             </div>
