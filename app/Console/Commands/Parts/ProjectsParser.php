3789fd90147e5339425331c6f115a99cdeb9c857
Kirill Vorotilov <kirill.vorotilov@greenice.net>
2021-02-08 12:21:46 +0300

    clean

--- app/Console/Commands/Parts/ProjectsParser.php
+++ app/Console/Commands/Parts/ProjectsParser.php
@@ -80,7 +80,6 @@ class ProjectsParser extends AbstractParser
     protected function getProjects()
     {
         $posts = $this->wpConnection->table('wp_posts')
-            ->where('id', 2060)
             ->join('wp_postmeta', 'wp_posts.id', '=', 'wp_postmeta.post_id')
             ->where('wp_posts.post_type', 'page')
             ->where('wp_posts.post_status', 'publish')
