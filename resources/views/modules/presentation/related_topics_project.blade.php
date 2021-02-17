25750a6af474bb2d52f579eedcbc2f0080d13633
vetal33 <vitalii.bytko@greenice.net>
2021-02-17 19:45:46 +0200

    fix related topic

--- resources/views/modules/presentation/related_topics_project.blade.php
+++ resources/views/modules/presentation/related_topics_project.blade.php
@@ -32,66 +32,83 @@
     }
 
     $pages = \App\Models\Project::getRelPages($pageIds);
-
+    $lastBlogPages = \App\Models\Page::lastBlogs(3);
 
 @endphp
 
-@if(!$pages->isEmpty())
-    <section class="discover-more @empty($bgClass) bg-white @else {{ $bgClass }} @endempty">
-    <div class="wrap">
-        <div class="title">
-            <div class="row">
-                <div class="col-7">
-                    <b class="font-size-30 mr-4 text-uppercase">
-                        @if ($relPageTitle === "")
-                            RELATED TOPICS
-                        @else
-                            {{ $relPageTitle }}
-                        @endif
-                    </b>
-                </div>
-                <div class="col-5 text-right pr-0">
-                    <a href="
-                    @if ($relPageLink === "")
-                        /newsroom
-@else
-                    {{ $relPageLink }}
+<section class="discover-more @empty($bgClass) bg-white @else {{ $bgClass }} @endempty">
+<div class="wrap">
+    <div class="title">
+        <div class="row">
+            <div class="col-7">
+                <b class="font-size-30 mr-4 text-uppercase">
+                    @if ($relPageTitle === "")
+                        RELATED TOPICS
+                    @else
+                        {{ $relPageTitle }}
                     @endif
-                        " class="text-uppercase text-underline ">
-                        <b>
-                            @if ($relPageLinkTitle === "")
-                                VISIT NEWSROOM
-                            @else
-                                {{ $relPageLinkTitle }}
-                            @endif
+                </b>
+            </div>
+            <div class="col-5 text-right pr-0">
+                <a href="
+                @if ($relPageLink === "")
+                    /newsroom
+                @else
+                {{ $relPageLink }}
+                @endif
+                    " class="text-uppercase text-underline ">
+                    <b>
+                        @if ($relPageLinkTitle === "")
+                            VISIT NEWSROOM
+                        @else
+                            {{ $relPageLinkTitle }}
+                        @endif
 
-                        </b> <i class="moon-icons-arrow-right"></i></a>
-                </div>
+                    </b> <i class="moon-icons-arrow-right"></i></a>
             </div>
         </div>
+    </div>
 
-        <div class="current-projects-list">
-            <div class="wrap">
-                <div class="row gutter-5">
+    <div class="current-projects-list">
+        <div class="wrap">
+            <div class="row gutter-5">
+                @if(!$pages->isEmpty())
                     @foreach ($pages as $page)
+                    <div class="col-4">
+                        <a href="{{ $page->slug }}" class="item">
+                            @isset($page->preview_img)
+                                <span class="img" style="background-repeat:no-repeat; background-image: url({{ url($page->preview_img) }})"></span>
+                            @else
+                                <span class="img" style="background: #eee"></span>
+                            @endisset
+
+                            <span class="descr">
+                            <span class="text font-size-16 text-uppercase">{{ \App\Helpers\StrHelper::lengthLimit($page->name, 20) }}</span>
+                            <span class="name font-size-16 "><b>{{ \App\Helpers\StrHelper::lengthLimit($page->preview_text, 60) }}</b></span>
+                            </span>
+                        </a>
+                    </div>
+                    @endforeach
+                @else
+                    @foreach ($lastBlogPages as $blog)
                         <div class="col-4">
-                            <a href="{{ url($page->slug) }}" class="item">
-                                @isset($page->preview_img)
-                                    <span class="img" style="background-repeat:no-repeat; background-image: url({{ url($page->preview_img) }})"></span>
+                            <a href="{{Request::root()}}/{{ $blog->getActualPageInstanceAttribute()->slug }}" class="item">
+                                @isset($blog->getActualPageInstanceAttribute()->preview_img)
+                                    <span class="img" style="background-repeat:no-repeat; background-image: url({{ $blog->getActualPageInstanceAttribute()->preview_img }})"></span>
                                 @else
                                     <span class="img" style="background: #eee"></span>
                                 @endisset
 
                                 <span class="descr">
-                                <span class="text font-size-16 text-uppercase">{{ \App\Helpers\StrHelper::lengthLimit($page->name, 20) }}</span>
-                                <span class="name font-size-16 "><b>{{ \App\Helpers\StrHelper::lengthLimit($page->preview_text, 60) }}</b></span>
-                                </span>
+                            <span class="text font-size-16 text-uppercase font-weight-normal">{{ \App\Helpers\StrHelper::lengthLimit($blog->getActualPageInstanceAttribute()->name, 20) }}</span>
+                            <span class="name font-size-16 "><b>{{ \App\Helpers\StrHelper::lengthLimit($blog->getActualPageInstanceAttribute()->parameters['hdr_text'], 60) }}</b></span>
+                            </span>
                             </a>
                         </div>
                     @endforeach
-                </div>
+                @endif
             </div>
         </div>
     </div>
+</div>
 </section>
-@endif
