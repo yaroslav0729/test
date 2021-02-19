0431a21ffbf177025a5dd5149706c0b9175552b5
vetal33 <vitalii.bytko@greenice.net>
2021-02-19 11:30:28 +0200

    fix mission possible module

--- app/Models/Template.php
+++ app/Models/Template.php
@@ -395,15 +395,6 @@ class Template
 
                 $rules = [
                     'parameters.per_page' => 'numeric|min:1',
-                    'parameters.need_title' => 'required',
-                    'parameters.need_text' => 'required|max:170',
-                    'parameters.need_link_text' => 'required',
-                    'parameters.need_link' => 'required',
-
-                    'parameters.need_photo' => 'required',
-                    'parameters.is_help_bg_color' => 'nullable|string',
-                    'parameters.is_help_btn_color' => 'nullable|string',
-
                 ];
 
                 break;
