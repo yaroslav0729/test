af5422be8287258927dbaacacdf93d85cf2b6dfa
vetal33 <vitalii.bytko@greenice.net>
2021-02-19 18:45:39 +0200

    fix search picture

--- resources/views/search/index.blade.php
+++ resources/views/search/index.blade.php
@@ -11,7 +11,14 @@
             <div class="col-12">
                 <div class="card flex-md-row mb-4 box-shadow h-md-200">
                     <img class="card-img-right flex-auto d-none d-md-block"
-                         data-src="holder.js/200x150?theme=thumb" alt="/storage/images/Praying-mosque-background.jpg" style="width: 250px; height: 200px;" src="{{ $page->getActualPageInstanceAttribute()->preview_img ?? '/storage/images/Praying-mosque-background.jpg' }} " data-holder-rendered="true">
+                         data-src="holder.js/200x150?theme=thumb" alt="/storage/images/Praying-mosque-background.jpg" style="width: 250px; height: 200px;"
+                    @if (empty($page->getActualPageInstanceAttribute()->preview_img))
+                         src="/storage/images/Praying-mosque-background.jpg"
+                    @else
+                         src="{{ $page->getActualPageInstanceAttribute()->preview_img }}"
+                    @endif
+              {{--      {{ $page->getActualPageInstanceAttribute()->preview_img ?? '/storage/images/Praying-mosque-background.jpg' }} "--}}
+                         data-holder-rendered="true">
                     <div class="card-body d-flex flex-column align-items-start">
                         <h3 class="mb-0">
                             <a class="text-dark" href="{{ $page->getActualPageInstanceAttribute()->slug ?? '' }}">{{ $page->getActualPageInstanceAttribute()->name ?? '' }}</a>
