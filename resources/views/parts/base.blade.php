d4dc92952fc56ecfea2ce92e8f4c972db011e533
Stanislav Panchenko <stanislav_panchenko@epam.com>
2024-04-12 14:33:55 +0300

    fixed package

--- resources/views/parts/base.blade.php
+++ resources/views/parts/base.blade.php
@@ -16,8 +16,12 @@
 
 
     <link href="libs/fontawesome-pro-5.14.0/css/all.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
-    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+    <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
+    />
+
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 
     <script src="libs/bootstrap-input-spinner.js"></script>
 
--- resources/views/parts/head.blade.php
+++ resources/views/parts/head.blade.php
@@ -15,8 +15,12 @@
 @include('parts.styles')
 <script src="{{ mix('js/app.js') }}"></script>
 <script src="{{ mix('js/scheduled-qurbani.js') }}"></script>
-<link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
-<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+<link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
+/>
+
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 <link rel="shortcut icon" href="{{ url('/') }}/favicon.ico">
 <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap"
     rel="stylesheet">
