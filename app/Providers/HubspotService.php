ef05983c1a44cec7cc2a00bd2c356f319bf23e64
andriivoron <andriivoron057@gmail.com>
2025-03-03 12:36:22 -0800

    updated hubspot property names

--- app/Providers/HubspotService.php
+++ app/Providers/HubspotService.php
@@ -116,7 +116,8 @@ class HubspotService
         $object['donation_date'] = $donation->created_at->timezone('UTC')->startOfDay()->timestamp . '000';
         $object['time'] = $donation->created_at->format('H:i:s');
         $object['campaign_name'] = $donation->campaign ? $donation->campaign->name : 'No campaign';
-        $object['program'] = $donation->campaign ? $donation->campaign->program_name : 'No program';
+        $object['program'] = $donation->campaign ? $donation->campaign->project_name : 'No program';
+        $object['project_category'] = $donation->campaign ? $donation->campaign->program_name : 'No project category';
         $object['donation_type'] = $donation->campaign_category ? $donation->campaign_category->name : 'General Donation';
         $object['gift_aid_'] = $donation->order && $donation->order->gift_aid ? true : false;
         $object['remark'] = $donation->order ? $donation->order->notes : '';
