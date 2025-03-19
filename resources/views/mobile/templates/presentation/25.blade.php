49e1b7b19ff38994fc2d23cd2c8f303e5cb047f0
andriivoron <andriivoron057@gmail.com>
2025-03-19 06:24:00 -0700

    hide map part

--- resources/views/mobile/templates/presentation/25.blade.php
+++ resources/views/mobile/templates/presentation/25.blade.php
@@ -243,7 +243,7 @@ if(strpos($requestPath, 'lebanon') !== false) {
 </section>
 
 @if(!$shouldHideMap)
-<section class="gw-map-btn">
+<section class="gw-map-btn" style="display: none;">
     <div style="background-image: url(img/who-we-are-map-mobile.jpg)" alt-src="{{ $mapAlternativeImage }}">
         <a href="#" id="btn-view-global-work" class="btn btn-info">View Global Work</a>
     </div>
