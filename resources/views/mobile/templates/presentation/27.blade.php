ffcc630bfe097cd8e392ede5ded584ed88b43e34
andrii <andriivoron057@gmail.com>
2025-05-06 09:46:21 -0300

    update mobile visible

--- resources/views/mobile/templates/presentation/27.blade.php
+++ resources/views/mobile/templates/presentation/27.blade.php
@@ -70,10 +70,10 @@ $videoRenderedBlock = \App\Models\Widget::replaceMonikers('{video-carousel|' . $
 
 <section class="donate-today" style="background-color: #291f4d;">
     <div class="wrap">
-        <div class="flex" style="display: flex; border-bottom: 5px solid #fff; padding: 0 10px;">
-            <div class="title" style="width: 50%; display: flex; align-items: center; justify-content: center;" project-title>
+        <div style="border-bottom: 5px solid #fff; padding: 0 10px; position: relative;">
+            <div class="title" style="display: flex; align-items: center; justify-content: center; width: 100%; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);" project-title>
                 <div style="display: flex; flex-direction: column;">
-                    <div style="color: #fac16a; font-size: 3rem;">
+                    <div style="color: #fac16a; font-size: 3rem; text-wrap: nowrap;">
                         @empty($projHeading)
                         <b>{{ $pageInstance->name }}</b>
                         @else
@@ -83,7 +83,7 @@ $videoRenderedBlock = \App\Models\Widget::replaceMonikers('{video-carousel|' . $
                     <div style="text-align: left; white-space: pre-wrap; color: #fff; font-size: 2rem; font-weight: 900;">{{ $projSubheading }}</div>
                 </div>
             </div>
-            <div class="media" style="width: 50%;">
+            <div class="media" style="width: 100%; margin: 0;">
                 <img src="{{ $projImage }}" style="width: 100%; height: 100%; object-fit: cover; max-height: 400px;">
             </div>
         </div>
