b1c315e014e5a5334273054616a4104d8156d4db
andrii <andriivoron057@gmail.com>
2025-05-15 18:45:19 -0300

    fix error

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -29,8 +29,8 @@ class PageController extends Controller
     {
         $indexPage = Page::published()->index()->first();
         $pageInstance = $indexPage->actual_page_instance;
-        if($pageInstance['title'])$pageInstance->title="Islamic Help";
-        if($pageInstance['description'])$pageInstance->description="Islamic Help";
+        if(isset($pageInstance['title']))$pageInstance->title="Islamic Help";
+        if(isset($pageInstance['description']))$pageInstance->description="Islamic Help";
 
         SEOMeta::setTitle($pageInstance->title);
         SEOMeta::setDescription($pageInstance->description);
