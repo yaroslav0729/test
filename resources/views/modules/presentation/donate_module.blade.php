8328e7b38712da3deab2950e68e8fc41dae7e977
andrii <andriivoron057@gmail.com>
2025-05-30 11:12:30 -0300

    add quantity text

--- resources/views/modules/presentation/donate_module.blade.php
+++ resources/views/modules/presentation/donate_module.blade.php
@@ -443,7 +443,10 @@ if (!isset($useAppeal)) {
                             <div class="pt-3"></div>
                             @if(!empty($parameters['countries_image']))
                             <div class="text-center pb-3">
-                                <a class="text-dark open-modal-countries-price">tap here to see all countries and their prices</a>
+                                <a class="text-dark open-modal-countries-price">tap here to see all countries and their prices</a><br>
+                                @if(strtolower($pageInstance->slug) === 'qurbani-2025-3')
+                                    <span>Select quantities in the cart after tapping Donate Now</span>
+                                @endif
                             </div>
                             @endif
                             <div class="text-center">
